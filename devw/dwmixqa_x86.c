acafba678a76bde1c92e3e719045d3ed951ebe08
mywave <stian.skjelstad@gmail.com>
2019-03-09 00:47:32 +0100

    devw/dwmixqa_x86.c:  * make assembler symbols appear in debugger  * force mixqPlayChannel to use frame-pointer (reference variables by ebp), since we use push/pop in the inline assembler  * use offsetof() instead of casting NULL into a struct pointer

--- devw/dwmixqa_x86.c
+++ devw/dwmixqa_x86.c
@@ -8,8 +8,19 @@ void noexternal_dwmixq(void)
 {
 	__asm__ __volatile__
 	(
+		".cfi_endproc\n"
+	);
+
+	__asm__ __volatile__
+	(
+		".type playquiet, @function\n"
 		"playquiet:\n"
+		".cfi_startproc\n"
+
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size playquiet, .-playquiet\n"
 	);
 
 /* esi = current source
@@ -25,7 +36,10 @@ void noexternal_dwmixq(void)
 
 	__asm__ __volatile__
 	(
+		".type playmono, @function\n"
 		"playmono:\n"
+		".cfi_startproc\n"
+
 		"  leal (%edi, %ecx, 2), %ecx\n"
 		"  xorb %bl, %bl\n"
 		"playmono_lp:\n"
@@ -37,11 +51,17 @@ void noexternal_dwmixq(void)
 		"  cmpl %ecx, %edi\n"
 		"  jb playmono_lp\n"
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size playquiet, .-playquiet\n"
 	);
 
 	__asm__ __volatile__
 	(
+		".type playmono16, @function\n"
 		"playmono16:\n"
+		".cfi_startproc\n"
+
 		"  leal (%edi, %ecx, 2), %ecx\n"
 		"  xorb %bl, %bl\n"
 		"playmono16_lp:\n"
@@ -53,11 +73,17 @@ void noexternal_dwmixq(void)
 		"  cmpl %ecx, %edi\n"
 		"  jb playmono16_lp\n"
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size playmono16, .-playmono16\n"
 	);
 
 	__asm__ __volatile__
 	(
+		".type playmonoi, @function\n"
 		"playmonoi:\n"
+		".cfi_startproc\n"
+
 		"  leal (%edi, %ecx, 2), %ecx\n"
 		"  movl %ecx, (playmonoi_endp-4)\n"
 		"  xorl %eax, %eax\n"
@@ -83,17 +109,29 @@ void noexternal_dwmixq(void)
 		"  jb playmonoi_lp\n"
 		"  ret\n"
 
+		".cfi_endproc\n"
+		".size playmonoi, .-playmonoi\n"
+
+		".type setupmonoi, @function\n"
 		"setupmonoi:\n"
+		".cfi_startproc\n"
+
 		"  movl %ebx, (playmonoi_intr1-4)\n"
 		"  addl $2, %ebx\n"
 		"  movl %ebx, (playmonoi_intr2-4)\n"
 		"  subl $2, %ebx\n"
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size setupmonoi, .-setupmonoi\n"
 	);
 
 	__asm__ __volatile__
 	(
+		".type playmonoi16, @function\n"
 		"playmonoi16:\n"
+		".cfi_startproc\n"
+
 		"  leal (%edi, %ecx, 2), %ecx\n"
 		"  movl %ecx, (playmonoi16_endp-4)\n"
 		"  movl %eax, %eax\n" /* pentium pipeline */
@@ -125,7 +163,14 @@ void noexternal_dwmixq(void)
 		"  jb playmonoi16_lp\n"
 		"  ret\n"
 
+		".cfi_endproc\n"
+		".size playmonoi16, .-playmonoi16\n"
+
+
+		".type setupmonoi16, @function\n"
 		"setupmonoi16:\n"
+		".cfi_startproc\n"
+
 		"  movl %ebx, (playmonoi16_intr1-4)\n"
 		"  addl $2, %ebx\n"
 		"  movl %ebx, (playmonoi16_intr2-4)\n"
@@ -135,11 +180,17 @@ void noexternal_dwmixq(void)
 		"  movl %ebx, (playmonoi16_intr3-4)\n"
 		"  subl $32768, %ebx\n" /* 4*32*256 - 2 */
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size setupmonoi16, .-setupmonoi16\n"
 	);
 
 	__asm__ __volatile__
 	(
+		".type playmonoi2, @function\n"
 		"playmonoi2:\n"
+		".cfi_startproc\n"
+
 		"  leal (%edi, %ecx, 2), %ecx\n"
 		"  movl %ecx, (playmonoi2_endp-4)\n"
 		"  movl %eax, %eax\n" /* pipeline */
@@ -171,7 +222,14 @@ void noexternal_dwmixq(void)
 
 		"  ret\n"
 
+		".cfi_endproc\n"
+		".size playmonoi2, .-playmonoi2\n"
+
+
+		".type setupmonoi2, @function\n"
 		"setupmonoi2:\n"
+		".cfi_startproc\n"
+
 		"  movl %ecx, (playmonoi2_intr1-4)\n"
 		"  addl $2, %ecx\n"
 		"  movl %ecx, (playmonoi2_intr2-4)\n"
@@ -179,11 +237,17 @@ void noexternal_dwmixq(void)
 		"  movl %ecx, (playmonoi2_intr3-4)\n"
 		"  subl $4, %ecx\n"
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size setupmonoi2, .-setupmonoi2\n"
 	);
 
 	__asm__ __volatile__
 	(
+		".type playmonoi216, @function\n"
 		"playmonoi216:\n"
+		".cfi_startproc\n"
+
 		"  leal (%edi, %ecx, 2), %ecx\n"
 		"  movl %ecx, (playmonoi216_endp-4)\n"
 		"  movl %ebx, %ecx\n"
@@ -228,7 +292,13 @@ void noexternal_dwmixq(void)
 		"  jb playmonoi216_lp\n"
 		"  ret\n"
 
+		".cfi_endproc\n"
+		".size playmonoi216, .-playmonoi216\n"
+
+		".type setupmonoi216, @function\n"
 		"setupmonoi216:\n"
+		".cfi_startproc\n"
+
 		"  movl %ecx, (playmonoi216_intr1-4)\n"
 		"  addl $2, %ecx\n"
 		"  movl %ecx, (playmonoi216_intr2-4)\n"
@@ -242,6 +312,14 @@ void noexternal_dwmixq(void)
 		"  movl %ecx, (playmonoi216_intr4-4)\n"
 		"  subl $32768, %ecx\n" /* 8*16*256 */
 		"  ret\n"
+
+		".cfi_endproc\n"
+		".size setupmonoi216, .-setupmonoi216\n"
+	);
+
+	__asm__ __volatile__
+	(
+		".cfi_startproc\n"
 	);
 }
 
@@ -250,6 +328,7 @@ void noexternal_dwmixq(void)
 static void *playrout;
 #endif
 
+__attribute__((optimize("-fno-omit-frame-pointer"))) /* we use the stack, so we need all access to go via EBP, not ESP */
 void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 {
 	int inloop;
@@ -257,6 +336,7 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 #ifdef __PIC__
 	void *playrout=NULL;
 #endif
+
 	__asm__ __volatile__
 	(
 #ifdef __PIC__
@@ -268,22 +348,22 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		"  cmpb $0, %3\n"                 /*  %3 = quiet */
 		"  jne mixqPlayChannel_pquiet\n"
 
-		"  testb %16, %6(%%edi)\n"        /* %16 = MIXQ_INTERPOLATE
+		"  testb %16, %c6(%%edi)\n"       /* %16 = MIXQ_INTERPOLATE
 		                                   *  %6 = ch->status */
 		"  jnz mixqPlayChannel_intr\n"
 		"    movl $playmono, %%eax\n"
-		"    testb %17, %6(%%edi)\n"      /* %17 = MIXQ_PLAY16BIT
+		"    testb %17, %c6(%%edi)\n"     /* %17 = MIXQ_PLAY16BIT
 		                                   *  %6 = ch->status */
 		"    jz mixqPlayChannel_intrfini\n"
 		"      movl $playmono16, %%eax\n"
 		"  jmp mixqPlayChannel_intrfini\n"
 		"mixqPlayChannel_intr:\n"
 
-		"  testb %18, %6(%%edi)\n"        /* %18 = MIXQ_INTERPOLATEMAX
+		"  testb %18, %c6(%%edi)\n"       /* %18 = MIXQ_INTERPOLATEMAX
 		                                   *  %6 = ch->status */
 		"  jnz mixqPlayChannel_intrmax\n"
 		"    movl $playmonoi, %%eax\n"
-		"    testb %17, %6(%%edi)\n"      /* %17 = MIXQ_PLAY16BIT
+		"    testb %17, %c6(%%edi)\n"     /* %17 = MIXQ_PLAY16BIT
 		                                   *  %6 = ch->status */
 		"    jz mixqPlayChannel_intrfini\n"
 		"      movl $playmonoi16, %%eax\n"
@@ -291,13 +371,14 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		"mixqPlayChannel_intrmax:\n"
 
 		"    movl $playmonoi2, %%eax\n"
-		"    testb %17, %6(%%edi)\n"      /* %17 = MIXQ_PLAY16BIT
+		"    testb %17, %c6(%%edi)\n"     /* %17 = MIXQ_PLAY16BIT
 		                                   *  %6 = ch->status */
 		"    jz mixqPlayChannel_intrfini\n"
 		"      movl $playmonoi216, %%eax\n"
 		" mixqPlayChannel_intrfini:\n"
 
 		"  movl %%eax, %7\n"              /*  %7 = playrout */
+
 		"  jmp mixqPlayChannel_bigloop\n"
 
 		"mixqPlayChannel_pquiet:\n"
@@ -307,35 +388,35 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		"mixqPlayChannel_bigloop:\n"
 
 		"  movl %1, %%ecx\n"              /*  %1 = len */
-		"  movl %8(%%edi), %%ebx\n"       /*  %8 = ch->step */
-		"  movl %9(%%edi), %%edx\n"       /*  %9 = ch->pos */
-		"  movw %10(%%edi), %%si\n"       /* %10 = ch->fpos */
+		"  movl %c8(%%edi), %%ebx\n"      /*  %8 = ch->step */
+		"  movl %c9(%%edi), %%edx\n"      /*  %9 = ch->pos */
+		"  movw %c10(%%edi), %%si\n"      /* %10 = ch->fpos */
 		"  movb $0, %4\n"                 /*  %4 = inloop */
 		"  cmpl $0, %%ebx\n"
 		"  je mixqPlayChannel_playecx\n"
 		"  jg mixqPlayChannel_forward\n"
 		"    negl %%ebx\n"
 		"    movl %%edx, %%eax\n"
-		"    testb %19, %6(%%edi)\n"      /* %19 = MIXQ_LOOPED
+		"    testb %19, %c6(%%edi)\n"     /* %19 = MIXQ_LOOPED
 		                                   *  %6 = ch->status */
 		"    jz mixqPlayChannel_maxplaylen\n"
-		"    cmpl %11(%%edi), %%edx\n"    /* %11 = ch->loopstart */
+		"    cmpl %c11(%%edi), %%edx\n"   /* %11 = ch->loopstart */
 		"    jb mixqPlayChannel_maxplaylen\n"
-		"    subl %11(%%edi), %%eax\n"    /* %11 = ch->loopstart */
+		"    subl %c11(%%edi), %%eax\n"   /* %11 = ch->loopstart */
 		"    movb $1, %4\n"               /*  %4 = inloop */
 		"    jmp mixqPlayChannel_maxplaylen\n"
 		"mixqPlayChannel_forward:\n"
 
-		"    movl %14(%%edi), %%eax\n"    /* %14 = ch->length */
+		"    movl %c14(%%edi), %%eax\n"   /* %14 = ch->length */
 		"    negw %%si\n"
 		"    sbbl %%edx, %%eax\n"
-		"    testb %19, %6(%%edi)\n"      /* %19 = MIXQ_LOOPED
+		"    testb %19, %c6(%%edi)\n"     /* %19 = MIXQ_LOOPED
 		                                   *  %6 = ch->status */
 		"    jz mixqPlayChannel_maxplaylen\n"
-		"    cmpl %12(%%edi), %%edx\n"    /* %12 = ch->loopend */
+		"    cmpl %c12(%%edi), %%edx\n"   /* %12 = ch->loopend */
 		"    jae mixqPlayChannel_maxplaylen\n"
-		"    subl %14(%%edi), %%eax\n"    /* %14 = ch->length */
-		"    addl %12(%%edi), %%eax\n"    /* %12 = ch->loopend */
+		"    subl %c14(%%edi), %%eax\n"   /* %14 = ch->length */
+		"    addl %c12(%%edi), %%eax\n"   /* %12 = ch->loopend */
 		"    movb $1, %4\n"               /*  %4 = inloop */
 
 
@@ -357,7 +438,7 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		"    movl %%eax, %%ecx\n"
 		"    cmpb $0, %4\n"               /*  %4 = inloop */
 		"    jnz mixqPlayChannel_playecx\n"
-		"      andb %20, %6(%%edi)\n"     /* %20 = MIXQ_PLAYING^255
+		"      andb %20, %c6(%%edi)\n"    /* %20 = MIXQ_PLAYING^255
 		                                   *  %6 = ch->status */
 		"      movl %1, %%eax\n"          /*  %1 = len */
 		"      subl %%ecx, %%eax\n"
@@ -375,17 +456,17 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		"  movl  %7, %%eax\n " /* step 1 */
 		"  pushl %%eax\n"      /* step 2 */
 #endif
-		"  movw %10(%%edi), %%bx\n"       /* %10 = ch->fpos */
+		"  movw %c10(%%edi), %%bx\n"      /* %10 = ch->fpos */
 		"  shll $16, %%ebx\n"
 		"  movl %0, %%eax\n"              /*  %0 = buf */
 
-		"  movl %8(%%edi), %%edx\n"       /*  %8 = ch->step */
+		"  movl %c8(%%edi), %%edx\n"      /*  %8 = ch->step */
 		"  shll $16, %%edx\n"
 
-		"  movl %9(%%edi), %%esi\n"       /*  %9 = ch->pos */
-		"  movl %8(%%edi), %%ebp\n"       /*  %8 = ch->step */
+		"  movl %c9(%%edi), %%esi\n"      /*  %9 = ch->pos */
+		"  movl %c8(%%edi), %%ebp\n"      /*  %8 = ch->step */
 		"  sarl $16, %%ebp\n"
-		"  addl %15(%%edi), %%esi\n"      /* %15 = ch->samp */
+		"  addl %c15(%%edi), %%esi\n"     /* %15 = ch->samp */
 		"  movl %%eax, %%edi\n"
 
 #ifdef __PIC__
@@ -402,61 +483,61 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		"  addl %%ecx, %0\n"              /*  %0 = buf */
 		"  subl %%ecx, %1\n"              /*  %1 = len */
 
-		"  movl %8(%%edi), %%eax\n"       /*  %8 = ch->step */
+		"  movl %c8(%%edi), %%eax\n"      /*  %8 = ch->step */
 		"  imul %%ecx\n"
 		"  shld $16, %%eax, %%edx\n"
-		"  addw %%ax, %10(%%edi)\n"       /* %10 = ch->fpos */
-		"  adcl %%edx, %9(%%edi)\n"       /*  %9 = ch->pos */
-		"  movl %9(%%edi), %%eax\n"       /*  %9 = ch->pos */
+		"  addw %%ax, %c10(%%edi)\n"      /* %10 = ch->fpos */
+		"  adcl %%edx, %c9(%%edi)\n"      /*  %9 = ch->pos */
+		"  movl %c9(%%edi), %%eax\n"      /*  %9 = ch->pos */
 
 		"  cmpb $0, %4\n"                 /*  %4 = inloop */
 		"  jz mixqPlayChannel_fill\n"
 
-		"  cmpl $0, %8(%%edi)\n"          /*  %8 = ch->step */
+		"  cmpl $0, %c8(%%edi)\n"         /*  %8 = ch->step */
 		"  jge mixqPlayChannel_forward2\n"
-		"    cmpl %11(%%edi), %%eax\n"    /* %11 = ch->loopstart */
+		"    cmpl %c11(%%edi), %%eax\n"   /* %11 = ch->loopstart */
 		"    jge mixqPlayChannel_exit\n"
 
-		"    testb %21, %6(%%edi)\n"      /* %21 = MIXQ_PINGPONGLOOP
+		"    testb %21, %c6(%%edi)\n"     /* %21 = MIXQ_PINGPONGLOOP
 		                                   *  %6 = ch->status */
 		"    jnz mixqPlayChannel_pong\n"
-		"      addl %13(%%edi), %%eax\n"  /* %13 = ch->replen */
+		"      addl %c13(%%edi), %%eax\n" /* %13 = ch->replen */
 		"      jmp mixqPlayChannel_loopiflen\n"
 		"    mixqPlayChannel_pong:\n"
 
-		"      negl %8(%%edi)\n"          /*  %8 = ch->step */
-		"      negw %10(%%edi)\n"         /* %10 = ch->fpos */
+		"      negl %c8(%%edi)\n"         /*  %8 = ch->step */
+		"      negw %c10(%%edi)\n"        /* %10 = ch->fpos */
 		"      adcl $0, %%eax\n"
 		"      negl %%eax\n"
-		"      addl %11(%%edi), %%eax\n"  /* %11 = ch->loopstart */
-		"      addl %11(%%edi), %%eax\n"  /* %11 = ch->loopstart */
+		"      addl %c11(%%edi), %%eax\n" /* %11 = ch->loopstart */
+		"      addl %c11(%%edi), %%eax\n" /* %11 = ch->loopstart */
 		"      jmp mixqPlayChannel_loopiflen\n"
 		"mixqPlayChannel_forward2:\n"
 
-		"    cmpl %12(%%edi), %%eax\n"    /* %12 = ch->loopend */
+		"    cmpl %c12(%%edi), %%eax\n"   /* %12 = ch->loopend */
 
 		"    jb mixqPlayChannel_exit\n"
 
-		"    testb %21, %6(%%edi)\n"      /* %21 = MIXQ_PINGPONGLOOP
+		"    testb %21, %c6(%%edi)\n"     /* %21 = MIXQ_PINGPONGLOOP
 		                                   *  %6 = ch->status */
 		"    jnz mixqPlayChannel_ping\n"
 
-		"      subl %13(%%edi), %%eax\n"  /* %13 = ch->replen */
+		"      subl %c13(%%edi), %%eax\n" /* %13 = ch->replen */
 
 		"      jmp mixqPlayChannel_loopiflen\n"
 
 		"    mixqPlayChannel_ping:\n"
 
-		"      negl %8(%%edi)\n"          /*  %8 = ch->step */
-		"      negw %10(%%edi)\n"         /* %10 = ch->fstep */
+		"      negl %c8(%%edi)\n"         /*  %8 = ch->step */
+		"      negw %c10(%%edi)\n"        /* %10 = ch->fstep */
 		"      adcl $0, %%eax\n"
 		"      negl %%eax\n"
-		"      addl %12(%%edi), %%eax\n"  /* %12 = ch->loopend */
-		"      addl %12(%%edi), %%eax\n"  /* %12 = ch->loopend */
+		"      addl %c12(%%edi), %%eax\n" /* %12 = ch->loopend */
+		"      addl %c12(%%edi), %%eax\n" /* %12 = ch->loopend */
 
 		"mixqPlayChannel_loopiflen:\n"
 
-		"  movl %%eax, %9(%%edi)\n"       /*  %9 = ch->pos */
+		"  movl %%eax, %c9(%%edi)\n"      /*  %9 = ch->pos */
 		"  cmpl $0, %1\n"                 /*  %1 = len */
 		"  jne mixqPlayChannel_bigloop\n"
 
@@ -464,10 +545,10 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 
 		"  cmpl $0, %5\n"                 /*  %5 = filllen */
 		"  je mixqPlayChannel_exit\n"
-		"  movl %14(%%edi), %%eax\n"      /* %14 = ch->length */
-		"  movl %%eax, %9(%%edi)\n"       /*  %9 = ch->pos */
-		"  addl %15(%%edi), %%eax\n"      /* %15 = ch->samp */
-		"  testb %17, %6(%%edi)\n"        /* %17 = MIXQ_PLAY16BIT
+		"  movl %c14(%%edi), %%eax\n"     /* %14 = ch->length */
+		"  movl %%eax, %c9(%%edi)\n"      /*  %9 = ch->pos */
+		"  addl %c15(%%edi), %%eax\n"     /* %15 = ch->samp */
+		"  testb %17, %c6(%%edi)\n"       /* %17 = MIXQ_PLAY16BIT
 		                                   *  %6 = ch->status */
 		"  jnz mixqPlayChannel_fill16\n"
 		"    movb (%%eax), %%ah\n"
@@ -490,16 +571,16 @@ void mixqPlayChannel(int16_t *buf, uint32_t len, struct channel *ch, int quiet)
 		  "m" (quiet),                               /*   3  */
 		  "m" (inloop),                              /*   4  */
 		  "m" (filllen),                             /*   5  */
-		  "m" (((struct channel *)NULL)->status),    /*   6  */
+		  "n" (offsetof(struct channel, status)),    /*   6  */
 		  "m" (playrout),                            /*   7  */
-		  "m" (((struct channel *)NULL)->step),      /*   8  */
-		  "m" (((struct channel *)NULL)->pos),       /*   9  */
-		  "m" (((struct channel *)NULL)->fpos),      /*  10  */
-		  "m" (((struct channel *)NULL)->loopstart), /*  11  */
-		  "m" (((struct channel *)NULL)->loopend),   /*  12  */
-		  "m" (((struct channel *)NULL)->replen),    /*  13  */
-		  "m" (((struct channel *)NULL)->length),    /*  14  */
-		  "m" (((struct channel *)NULL)->samp),      /*  15  */
+		  "n" (offsetof(struct channel, step)),      /*   8  */
+		  "n" (offsetof(struct channel, pos)),       /*   9  */
+		  "n" (offsetof(struct channel, fpos)),      /*  10  */
+		  "n" (offsetof(struct channel, loopstart)), /*  11  */
+		  "n" (offsetof(struct channel, loopend)),   /*  12  */
+		  "n" (offsetof(struct channel, replen)),    /*  13  */
+		  "n" (offsetof(struct channel, length)),    /*  14  */
+		  "n" (offsetof(struct channel, samp)),      /*  15  */
 		  "n" (MIXQ_INTERPOLATE),                    /*  16  */
 		  "n" (MIXQ_PLAY16BIT),                      /*  17  */
 		  "n" (MIXQ_INTERPOLATEMAX),                 /*  18  */
@@ -575,6 +656,7 @@ void mixqAmplifyChannel(int32_t *buf, int16_t *src, uint32_t len, int32_t vol, u
 void mixqAmplifyChannelUp(int32_t *buf, int16_t *src, uint32_t len, int32_t vol, uint32_t step)
 {
 	int d0, d1, d2, d3;
+
 	__asm__ __volatile__
 	(
 #ifdef __PIC__
