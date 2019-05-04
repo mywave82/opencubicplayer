615d9a862c3492cfb3c95924a9c49dfbb007bf40
Stian Skjelstad <stian.skjelstad@gmail.com>
2019-05-04 22:51:36 +0200

    Sorry for all the mess in this file. Last commit

--- stuff/poutput-fb.c
+++ stuff/poutput-fb.c
@@ -335,7 +335,7 @@ int fb_init(int minor)
 	fprintf(stderr, "fb:  mmio_start=0x%08lx\n", fix.mmio_start);
 	fprintf(stderr, "fb:  mmio_len=0x%08x\n", fix.mmio_len);
 	fprintf(stderr, "fb:  accel=%d\n", fix.accel);
-#if LINUX_VERSION >= KERNEL_VERSION (3,3,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,3,0)
 	fprintf(stderr, "fb:  capabilities=0x%04x\n", fix.capabilities);
 	fprintf(stderr, "fb:  reserved0=0x%04x reserved1=0x%04x\n", fix.reserved[0], fix.reserved[1]);
 #else
