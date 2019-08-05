988554624aa44e609ec15c036b2e14916f6280e8
Stian Skjelstad <stian.skjelstad@gmail.com>
2019-08-05 17:38:17 +0200

    Dump instrument-headers too

--- playgmd/dumps3m.c
+++ playgmd/dumps3m.c
@@ -57,6 +57,447 @@ void DumpPrefix (unsigned char *mem, int len, int base, int baselen)
 	}
 }
 
+int DumpInstrumentEmpty (unsigned char *mem, int len, int base, int instrument)
+{
+	int i;
+	printf ("[%sINSTRUMENT %d%s]\n", FONT_BRIGHT_CYAN, instrument + 1, FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x00, 1);
+	printf ("Type: empty instrument (message only)\n");
+
+	DumpPrefix (mem, len, base + 0x01, 12);
+	printf ("FileName: \"");
+	for (i=0; i < 12; i++)
+	{
+		if (!mem[base + 0x01 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x01 + i]);
+	}
+	printf("\"\n");
+
+	DumpPrefix (mem, len, base + 0x0d, 3+16+16);
+	printf ("Reserved / not used\n");
+
+	DumpPrefix (mem, len, base + 0x30, 28);
+	printf ("Message: \"");
+	for (i=0; i < 28; i++)
+	{
+		if (!mem[base + 0x30 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x30 + i]);
+	}
+	if (i == 28)
+	{
+		printf("\" %sWarning, no NUL terminator%s\n", FONT_BRIGHT_RED, FONT_RESET);
+	} else {
+		printf("\"\n");
+	}
+
+	DumpPrefix (mem, len, base + 0x4c, 4);
+	printf ("Magic: \"%c%c%c%c\" %s%s%s\n",
+		(char)mem[base + 0x4c + 0],
+		(char)mem[base + 0x4c + 1],
+		(char)mem[base + 0x4c + 2],
+		(char)mem[base + 0x4c + 3],
+		(memcmp (mem + base + 0x4c, "SCRI", 4) && memcmp (mem + base + 0x4c, "SCRS", 4)) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN,
+		(memcmp (mem + base + 0x4c, "SCRI", 4) && memcmp (mem + base + 0x4c, "SCRS", 4)) ? "Failed" : "OK",
+		FONT_RESET);
+
+	return 0;
+}
+
+int DumpInstrumentPCM (unsigned char *mem, int len, int base, int instrument)
+{
+	uint32_t Length    = uint32_little (((uint32_t *)(mem + base + 0x10))[0]);
+	uint32_t LoopStart = uint32_little (((uint32_t *)(mem + base + 0x14))[0]);
+	uint32_t LoopEnd   = uint32_little (((uint32_t *)(mem + base + 0x18))[0]);
+	uint32_t C2Spd     = uint32_little (((uint32_t *)(mem + base + 0x20))[0]);
+	int i;
+
+	printf ("[%sINSTRUMENT %d  / PCM Sample%s]\n", FONT_BRIGHT_CYAN, instrument + 1, FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x00, 1);
+	printf ("Type: PCM Sample\n");
+
+	DumpPrefix (mem, len, base + 0x01, 12);
+	printf ("FileName: \"");
+	for (i=0; i < 12; i++)
+	{
+		if (!mem[base + 0x01 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x01 + i]);
+	}
+	printf("\"\n");
+
+	DumpPrefix (mem, len, base + 0x0d, 3);
+	printf ("MemSeg: 0x%08x\n", mem[base+0x0d]<<16 | (mem[base+0x0e]<<8) | mem[base+0x0f]);
+
+	DumpPrefix (mem, len, base + 0x10, 4);
+	printf ("Length:     %d %s%s%s\n", (int)Length, Length>64000 ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (Length>64000) ? " Sample too long, will be cropped at 64000" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x14, 4);
+	printf ("Loop-Start: %d %s%s%s\n", (int)LoopStart, (LoopStart>Length) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (LoopStart>Length) ? " LoopStart > Length" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x18, 4);
+	printf ("Loop-End: %d %s%s%s\n", (int)LoopEnd, (LoopEnd>(Length+1)) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (LoopEnd>(Length+1)) ? " LoopEnd > Length + 1" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x1c, 1);
+	printf ("Volume: %d %s%s%s\n", mem[base+0x1c], (mem[base+0x1c] > 64) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (mem[base+0x1c] > 64) ? " This is greater than 64" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x1d, 1);
+	printf ("Reserved/unused\n");
+
+	DumpPrefix (mem, len, base + 0x1e, 1);
+	switch (mem[base + 0x1e])
+	{
+		case 0: printf ("unpacked)\n"); break;
+		case 1: printf ("DP30ADPCM packing (deprecated after Scream Tracker 3.00)\n"); break;
+		default: printf ("%sUnknown%s\n", FONT_BRIGHT_RED, FONT_RESET);
+	}
+
+	DumpPrefix (mem, len, base + 0x1f, 1);
+	printf ("Flags:\n");
+	printf ("                   1: [%c] Loop On\n", (mem[base + 0x1f] & 1) ? 'x' : ' ');
+	printf ("                   2: [%c] Stereo (Not support by ScreamTracker 3.01)\n",  (mem[base + 0x1f] & 2) ? 'x' : ' ');
+	printf ("                   4: [%c] 16-bit (Not support by ScreamTracker 3.01)\n", (mem[base + 0x1f] & 4) ? 'x' : ' ');
+
+	DumpPrefix (mem, len, base + 0x20, 4);
+	printf ("C2 Sample Rate: %d%s%s%s\n", (int)C2Spd, (C2Spd>0xffff)?FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (C2Spd>0xffff) ? " Scream Tracker 3 only supports upto 65535" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x24, 12);
+	printf ("Reserved / Internal use / leave at 0\n");
+
+	DumpPrefix (mem, len, base + 0x30, 28);
+	printf ("Sample Name: \"");
+	for (i=0; i < 28; i++)
+	{
+		if (!mem[base + 0x30 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x30 + i]);
+	}
+	if (i == 28)
+	{
+		printf("\" %sWarning, no NUL terminator%s\n", FONT_BRIGHT_RED, FONT_RESET);
+	} else {
+		printf("\"\n");
+	}
+
+	DumpPrefix (mem, len, base + 0x3c, 4);
+	printf ("Magic: \"%c%c%c%c\" %s%s%s\n",
+		(char)mem[base + 0x4c + 0],
+		(char)mem[base + 0x4c + 1],
+		(char)mem[base + 0x4c + 2],
+		(char)mem[base + 0x4c + 3],
+		(memcmp(mem + base + 0x4c, "SCRS", 4)) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN,
+		(memcmp(mem + base + 0x4c, "SCRS", 4)) ? "Failed" : "OK",
+		FONT_RESET);
+
+	return 0;
+}
+
+int DumpInstrumentAdlibMelody (unsigned char *mem, int len, int base, int instrument)
+{
+	uint32_t C2Spd     = uint32_little (((uint32_t *)(mem + base + 0x20))[0]);
+	int i;
+
+	printf ("[%sINSTRUMENT %d  / Adlib Melody%s]\n", FONT_BRIGHT_CYAN, instrument + 1, FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x00, 1);
+	printf ("Type: Adlib Melody\n");
+
+	DumpPrefix (mem, len, base + 0x01, 12);
+	printf ("FileName: \"");
+	for (i=0; i < 12; i++)
+	{
+		if (!mem[base + 0x01 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x01 + i]);
+	}
+	printf("\"\n");
+
+	DumpPrefix (mem, len, base + 0x0d, 3);
+	printf ("Reserved\n");
+
+	DumpPrefix (mem, len, base + 0x10, 1);
+	printf ("D00 (adlib reg 0x23): Modulator -- Tremolo:%d Vibrator:%d Sustain:%d EnvelopeScaling:%d  FrequencyMultiplicationFactor:%d (0=half)\n",
+		!!(mem[base+0x10]&0x80),
+		!!(mem[base+0x10]&0x40),
+		!!(mem[base+0x10]&0x20),
+		!!(mem[base+0x10]&0x10),
+		mem[base+0x10]&0x0f);
+
+	DumpPrefix (mem, len, base + 0x11, 1);
+	printf ("D01 (adlib reg 0x23):  Carrier  -- Tremolo:%d Vibrator:%d Sustain:%d EnvelopeScaling:%d  FrequencyMultiplicationFactor:%d (0=half)\n",
+		!!(mem[base+0x11]&0x80),
+		!!(mem[base+0x11]&0x40),
+		!!(mem[base+0x11]&0x20),
+		!!(mem[base+0x11]&0x10),
+		mem[base+0x11]&0x0f);
+
+	DumpPrefix (mem, len, base + 0x12, 1);
+	printf ("D02 (adlib reg 0x43): Modulator -- KeyScaleLevel:%d OutputLevel:%d (inverse)\n",
+		mem[base+0x12] >> 6,
+		mem[base+0x12] & 0x3f);
+
+	DumpPrefix (mem, len, base + 0x13, 1);
+	printf ("D03 (adlib reg 0x43):  Carrier  -- KeyScaleLevel:%d OutputLevel:%d (inverse)\n",
+		mem[base+0x13] >> 6,
+		mem[base+0x13] & 0x3f);
+
+	DumpPrefix (mem, len, base + 0x14, 1);
+	printf ("D04 (adlib reg 0x60): Modulator -- AttackRate:%d DecayRate:%d\n",
+		mem[base+0x14] >> 4,
+		mem[base+0x14] & 0x0f);
+
+	DumpPrefix (mem, len, base + 0x15, 1);
+	printf ("D05 (adlib reg 0x63):  Carrier  -- AttackRate:%d DecayRate:%d\n",
+		mem[base+0x15] >> 4,
+		mem[base+0x15] & 0x0f);
+
+	DumpPrefix (mem, len, base + 0x16, 1);
+	printf ("D06 (adlib reg 0x80): Modulator -- SustainLevel:%d ReleaseRate:%d\n",
+		mem[base+0x16] >> 4,
+		mem[base+0x16] & 0x0f);
+
+	DumpPrefix (mem, len, base + 0x17, 1);
+	printf ("D07 (adlib reg 0x83):  Carrier  -- SustainRate:%d ReleaseRate:%d\n",
+		mem[base+0x17] >> 4,
+		mem[base+0x17] & 0x0f);
+
+	DumpPrefix (mem, len, base + 0x18, 1);
+	printf ("D08 (adlib reg 0xe0): Modulator -- WaveForm:");
+	switch (mem[base+0x18] & 7)
+	{
+		case 0: printf ("Sine\n");
+		case 1: printf ("Half-Sine\n");
+		case 2: printf ("Abs-Sine\n");
+		case 3: printf ("Pulse-Sine\n");
+		case 4: printf ("Sine - even periods only (OPL3 only)\n");
+		case 5: printf ("Abs-Sine - even periods only (OPL3 only)\n");
+		case 6: printf ("Square (OPL3 only)\n");
+		case 7: printf ("Derived Square (OPL3 only)\n");
+	}
+
+	DumpPrefix (mem, len, base + 0x19, 1);
+	printf ("D09 (adlib reg 0xe3):  Carrier  -- WaveForm:");
+	switch (mem[base+0x19] & 7)
+	{
+		case 0: printf ("Sine\n");
+		case 1: printf ("Half-Sine\n");
+		case 2: printf ("Abs-Sine\n");
+		case 3: printf ("Pulse-Sine\n");
+		case 4: printf ("Sine - even periods only (OPL3 only)\n");
+		case 5: printf ("Abs-Sine - even periods only (OPL3 only)\n");
+		case 6: printf ("Square (OPL3 only)\n");
+		case 7: printf ("Derived Square (OPL3 only)\n");
+	}
+
+	DumpPrefix (mem, len, base + 0x1a, 1);
+	printf ("D0A (adlib reg 0xc0): Modulator -- ModulatationFeedback:%d SynthesisType:%s\n",
+		(mem[base + 0x1a] >> 1 ) & 7,
+		(mem[base + 0x1a] & 0x01) ? "Additive synthesis":"Frequency Modulation");
+
+	DumpPrefix (mem, len, base + 0x1b, 1);
+	printf ("D0B Unused\n");
+
+	DumpPrefix (mem, len, base + 0x1c, 1);
+	printf ("Volume: %d %s%s%s\n", mem[base+0x1c], (mem[base+0x1c] > 64) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (mem[base+0x1c] > 64) ? " This is greater than 64" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x1e, 2);
+	printf ("Reserved/unused\n");
+
+	DumpPrefix (mem, len, base + 0x20, 4);
+	printf ("C2 Sample Rate: %d%s%s%s\n", (int)C2Spd, (C2Spd>0xffff)?FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (C2Spd>0xffff) ? " Scream Tracker 3 only supports upto 65535" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x24, 12);
+	printf ("Reserved / Internal use / leave at 0\n");
+
+	DumpPrefix (mem, len, base + 0x30, 28);
+	printf ("Sample Name: \"");
+	for (i=0; i < 28; i++)
+	{
+		if (!mem[base + 0x30 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x30 + i]);
+	}
+	if (i == 28)
+	{
+		printf("\" %sWarning, no NUL terminator%s\n", FONT_BRIGHT_RED, FONT_RESET);
+	} else {
+		printf("\"\n");
+	}
+
+	DumpPrefix (mem, len, base + 0x3c, 4);
+	printf ("Magic: \"%c%c%c%c\" %s%s%s\n",
+		(char)mem[base + 0x4c + 0],
+		(char)mem[base + 0x4c + 1],
+		(char)mem[base + 0x4c + 2],
+		(char)mem[base + 0x4c + 3],
+		(memcmp(mem + base + 0x4c, "SCRI", 4)) ? FONT_BRIGHT_RED: FONT_BRIGHT_GREEN,
+		(memcmp(mem + base + 0x4c, "SCRI", 4)) ? "Failed" : "OK",
+		FONT_RESET);
+
+	return 0;
+}
+
+int DumpInstrumentAdlibDrum (unsigned char *mem, int len, int base, int instrument)
+{
+	uint32_t C2Spd     = uint32_little (((uint32_t *)(mem + base + 0x20))[0]);
+	int i;
+
+	printf ("[%sINSTRUMENT %d  / Adlib Percussive%s]\n", FONT_BRIGHT_CYAN, instrument + 1, FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x00, 1);
+	printf ("Type: Adlib percussive:");
+	switch (mem[base+0x00])
+	{
+		case 3: printf ("bass drum\n"); break;
+		case 4: printf ("snare\n"); break;
+		case 5:	printf ("tom tom\n"); break;
+		case 6: printf ("top cymbal\n"); break;
+		case 7: printf ("hi-hat\n"); break;
+	}
+
+	DumpPrefix (mem, len, base + 0x01, 12);
+	printf ("FileName: \"");
+	for (i=0; i < 12; i++)
+	{
+		if (!mem[base + 0x01 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x01 + i]);
+	}
+	printf("\"\n");
+
+	DumpPrefix (mem, len, base + 0x0d, 3 + 12);
+	printf ("Reserved\n");
+
+	DumpPrefix (mem, len, base + 0x1c, 1);
+	printf ("Volume: %d %s%s%s\n", mem[base+0x1c], (mem[base+0x1c] > 64) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (mem[base+0x1c] > 64) ? " This is greater than 64" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x1e, 2);
+	printf ("Reserved/unused\n");
+
+	DumpPrefix (mem, len, base + 0x20, 4);
+	printf ("C2 Sample Rate: %d%s%s%s\n", (int)C2Spd, (C2Spd>0xffff)?FONT_BRIGHT_RED : FONT_BRIGHT_GREEN, (C2Spd>0xffff) ? " Scream Tracker 3 only supports upto 65535" : "", FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x24, 12);
+	printf ("Reserved / Internal use / leave at 0\n");
+
+	DumpPrefix (mem, len, base + 0x30, 28);
+	printf ("Sample Name: \"");
+	for (i=0; i < 28; i++)
+	{
+		if (!mem[base + 0x30 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x30 + i]);
+	}
+	if (i == 28)
+
+	{
+		printf("\" %sWarning, no NUL terminator%s\n", FONT_BRIGHT_RED, FONT_RESET);
+	} else {
+		printf("\"\n");
+	}
+
+	DumpPrefix (mem, len, base + 0x4c, 4);
+	printf ("Magic: \"%c%c%c%c\" %s%s%s\n",
+		(char)mem[base + 0x4c + 0],
+		(char)mem[base + 0x4c + 1],
+		(char)mem[base + 0x4c + 2],
+		(char)mem[base + 0x4c + 3],
+		(memcmp(mem + base + 0x4c, "SCRI", 4)) ? FONT_BRIGHT_RED : FONT_BRIGHT_GREEN,
+		(memcmp(mem + base + 0x4c, "SCRI", 4)) ? "Failed" : "OK",
+		FONT_RESET);
+
+	return 0;
+}
+
+int DumpInstrumentUnknown (unsigned char *mem, int len, int base, int instrument)
+{
+	int i;
+	printf ("[%sINSTRUMENT %d / Unknown%s]\n", FONT_BRIGHT_CYAN, instrument + 1, FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x00, 1);
+	printf ("Type: %sUnknown%s\n", FONT_BRIGHT_RED, FONT_RESET);
+
+	DumpPrefix (mem, len, base + 0x01, 12);
+	printf ("FileName: \"");
+	for (i=0; i < 12; i++)
+	{
+		if (!mem[base + 0x01 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x01 + i]);
+	}
+	printf("\n");
+
+	DumpPrefix (mem, len, base + 0x0d, 3+16+16);
+	printf ("Reserved / not used\n");
+
+	DumpPrefix (mem, len, base + 0x30, 28);
+	printf ("Message: \"");
+	for (i=0; i < 28; i++)
+	{
+		if (!mem[base + 0x30 + i])
+		{
+			break;
+		}
+		putchar (mem[base + 0x30 + i]);
+	}
+	if (i == 28)
+	{
+		printf(" %sWarning, no NUL terminator%s", FONT_BRIGHT_RED, FONT_RESET);
+	}
+	printf("\n");
+
+	DumpPrefix (mem, len, base + 0x4c, 4);
+	printf ("Magic: \"%c%c%c%c\" ???\n",
+		(char)mem[base + 0x4c + 0],
+		(char)mem[base + 0x4c + 1],
+		(char)mem[base + 0x4c + 2],
+		(char)mem[base + 0x4c + 3]);
+
+	return 0;
+}
+
+int DumpInstrument (unsigned char *mem, int len, int base, int instrument)
+{
+	if (base+0x50 > len)
+	{
+		fprintf (stderr, "0x%08x + 0x50 > filelen, unable to print instrument %d\n", base, instrument + 1);
+		return -1;
+	}
+	switch (mem[base])
+	{
+		case 0: return DumpInstrumentEmpty (mem, len, base, instrument); break;
+		case 1: return DumpInstrumentPCM (mem, len, base, instrument); break;
+		case 2: return DumpInstrumentAdlibMelody (mem, len, base, instrument); break;
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7: return DumpInstrumentAdlibDrum (mem, len, base, instrument); break;
+		default: return DumpInstrumentUnknown (mem, len, base, instrument); break;
+	}
+}
+
 int ParseS3M (unsigned char *mem, int len)
 {
 	int i;
@@ -320,6 +761,12 @@ int ParseS3M (unsigned char *mem, int len)
 		}
 	}
 
+	for (i=0; i < hdr.ins; i++)
+	{
+		uint16_t temp = uint16_little (((uint16_t *)(mem + sizeof(hdr) + hdr.orders))[i]);
+		DumpInstrument (mem, len, 16*temp, i);
+	}
+
 	return 0;
 }
 
@@ -375,9 +822,11 @@ int main(int argc, char *argv[])
 	if (!strcmp (color, "auto"))
 	{
 		usecolor = isatty ( 1 );
-	} else if ((!strcmp (color, "never")) || (!strcmp (color, "no")))
+	} else if ((strcmp (color, "never")) && (strcmp (color, "no")))
 	{
 		usecolor = 1;
+	} else {
+		usecolor = 0;
 	}
 
 	if (help)
