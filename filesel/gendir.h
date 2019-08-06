783edf590ee1b698271b16ee9ccdb2785490d832
Stian Skjelstad <stian.skjelstad@gmail.com>
2019-08-06 23:40:23 +0200

    Remove gendir() and genreldir()

--- filesel/gendir.h
+++ filesel/gendir.h
@@ -1,10 +1,6 @@
 #ifndef _GENDIR_HELPERS_H
 #define _GENDIR_HELPERS_H
 
-/* TO BE REMOVED */ void gendir(const char *orgdir, const char *fixdir, char *targetdir);
-/* TO BE REMOVED */ void genreldir(const char *orgdir, const char *fixdir, char *targetdir);
-
-
 int gendir_malloc (const char *basepath, const char *relpath, char **resultpath);
 
 int genreldir_malloc(const char *basepath, const char *targetpath, char **relpath);
