a47c2b594b0fe2ffd712f9c4ef3e6ba3c5695629
Stian Skjelstad <stian.skjelstad@gmail.com>
2019-11-30 00:21:51 +0100

    The correct macro to use is PRId64, not __PRI64_PREFIX   (Caused problems on systems that are not pure glibc)

--- playogg/oggplay.c
+++ playogg/oggplay.c
@@ -136,7 +136,7 @@ static void oggIdler(void)
 		}
 		if (ov_pcm_tell(&ov)!=oggpos)
 		{
-			fprintf (stderr, "[playogg]: warning, frame position is broken in file (got=%" __PRI64_PREFIX "d, expected= %" __PRI64_PREFIX "d)\n", ov_pcm_tell(&ov), oggpos);
+			fprintf (stderr, "[playogg]: warning, frame position is broken in file (got=%" PRId64 ", expected= %" PRId64 ")\n", ov_pcm_tell(&ov), oggpos);
 		}
 
 		ringbuffer_get_head_samples (oggbufpos, &pos1, &length1, &pos2, &length2);
