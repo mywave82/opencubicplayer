eff4833bbf6032d9f853313af2f3b477626d769c
Stian Skjelstad <stian.skjelstad@gmail.com>
2021-02-03 14:36:43 +0100

    Earlier changes to framelock.h removed an extern keyword too much

--- stuff/framelock.h
+++ stuff/framelock.h
@@ -6,6 +6,6 @@ void framelock(void); /* sleeps until next timeout */
 int poll_framelock(void); /* returns non-zero at framelock intervals, to be used for culling when I/O is expected to be high */
 void preemptive_framelock (void); /* called by I/O intensive sections, but return return value is delayed until the next call to poll_framelock */
 
-int fsFPS;
+extern int fsFPS;
 
 #endif
