ea3387444ba78d0df58c3db428ab1415fd586ff6
Stian Skjelstad <stian.skjelstad@gmail.com>
2022-01-18 09:47:01 +0100

    Create display_nprintf(). This makes is possible to combine multiple displaychr() displaystr() displaystr_utf() into a single encoded statement

--- boot/console.c
+++ boot/console.c
@@ -24,6 +24,8 @@
  */
 
 #include "config.h"
+#include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
 #include "types.h"
 #include "console.h"
@@ -36,15 +38,16 @@ const char *(*_plGetDisplayTextModeName)(void);
 
 void displaychr (const uint16_t y, const uint16_t x, const uint8_t attr, const char chr, const uint16_t len)
 {
+#define DISPLAYCHR_LEN 256
 	int i;
-	char buffer[16];
+	char buffer[DISPLAYCHR_LEN];
 	if (!len) return;
-	memset (buffer, chr, 16);
-	for (i=0; i * 16 < len; i++)
+	memset (buffer, chr, DISPLAYCHR_LEN);
+	for (i=0; i * DISPLAYCHR_LEN < len; i++)
 	{
-		int l = len - i * 16;
-		if (l > 16) l = 16;
-		_displaystr (y, x + i * 16, attr, buffer, l);
+		int l = len - i * DISPLAYCHR_LEN;
+		if (l > DISPLAYCHR_LEN) l = DISPLAYCHR_LEN;
+		_displaystr (y, x + i * DISPLAYCHR_LEN, attr, buffer, l);
 	}
 }
 
@@ -90,3 +93,480 @@ uint8_t *plVidMem;
 int plScrTextGUIOverlay;
 void *(*plScrTextGUIOverlayAddBGRA)(unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned int pitch, uint8_t *data_bgra);
 void (*plScrTextGUIOverlayRemove)(void *handle);
+
+void display_nprintf (unsigned short y, unsigned short x, unsigned char color, unsigned short width, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	while ((*fmt) && width)
+	{
+		int spaceflag = 0;
+		int minusflag = 0;
+		int plusflag = 0;
+		int zeroflag = 0;
+		int hashflag = 0;
+		int requested_width = INT_MAX;
+		int requested_precision = INT_MAX;
+		unsigned long long llu;
+		signed long long lls;
+		unsigned char c;
+
+		if (fmt[0] != '%')
+		{
+			const char *next = strchr (fmt, '%');
+			int len;
+			if (!next)
+			{
+				next = fmt + strlen (fmt);
+			}
+			len = next - fmt;
+			if (len > width)
+			{
+				len = width;
+			}
+			_displaystr (y, x, color, fmt, len);
+			x += len;
+			width -= len;
+			fmt = next;
+			continue;
+		}
+		fmt++;
+		if ((*fmt) == '%')
+		{
+			displaychr (y, x, color, '%', 1);
+			x++;
+			width--;
+			fmt++;
+			continue;
+		}
+before_dot:
+		if (!*fmt)
+		{
+			break;
+		}
+		switch (*fmt)
+		{
+			case '-':
+				minusflag = 1;
+				fmt++;
+				goto before_dot;
+			case '+':
+				plusflag = 1;
+				fmt++;
+				goto before_dot;
+			case ' ':
+				spaceflag = 1;
+				fmt++;
+				goto before_dot;
+			case '#':
+				hashflag = 1;
+				fmt++;
+				goto before_dot;
+			case '*':
+				requested_width = va_arg(ap, int);
+				fmt++;
+				goto before_dot;
+			case '.':
+				fmt++;
+				goto after_dot;
+			case '0':
+				zeroflag = 1;
+				fmt++;
+				goto before_dot;
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				requested_width = strtol (fmt, (char **)&fmt, 10);
+				goto before_dot;
+			default:
+				break; /* fall into the after dot loop */
+		}
+after_dot:
+		if (!*fmt)
+		{
+			break;
+		}
+		switch (*fmt)
+		{
+			default:
+				va_end(ap);
+				return; /* FAILURE */
+			case '*':
+				requested_precision = va_arg(ap, int);
+				fmt++;
+				goto after_dot;
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				requested_precision = strtol (fmt, (char **)&fmt, 10);
+				goto after_dot;
+			case 'C':
+				if (!fmt[1])
+				{
+					va_end (ap);
+					return;
+				}
+				fmt++;
+				c = *fmt;
+				goto got_c; /* got_c will to the last fmt++ */
+			case 'c':
+				{
+					c = (unsigned char)va_arg (ap, int);
+				got_c:
+					if (requested_width == INT_MAX)
+					{
+						requested_width = 1;
+					}
+					if (requested_precision==INT_MAX)
+					{
+						requested_precision = requested_width;
+					}
+					if (requested_precision > width)
+					{
+						requested_precision = width;
+					}
+					if (requested_width > width)
+					{
+						requested_width = width;
+					}
+					if (requested_precision > requested_width)
+					{
+						requested_precision = requested_width;
+					}
+					if ((requested_width > requested_precision) && minusflag)
+					{
+						displaychr (y, x, color, ' ', requested_width - requested_precision);
+						x += requested_width - requested_precision;
+					}
+					displaychr (y, x, color, c, requested_precision);
+					x += requested_precision;
+					if ((requested_width > requested_precision) && !minusflag)
+					{
+						displaychr (y, x, color, ' ', requested_width - requested_precision);
+						x += requested_width - requested_precision;
+					}
+					width -= requested_width;
+					fmt++;
+					break;
+				}
+			case 'o':
+				if (requested_width == INT_MAX)
+				{
+					if (zeroflag)
+					{
+						color &= 0x0f;
+					}
+				} else {
+					color &= 0x0f;
+					color |= requested_width << 4;
+				}
+				if (requested_precision != INT_MAX)
+				{
+					color &= 0xf0;
+					color |= (requested_precision & 15);
+				}
+				fmt++;
+				break;
+			case 's':
+				{
+					const char *src = va_arg (ap, const char *);
+
+					if (requested_precision==INT_MAX)
+					{
+						requested_precision = strlen (src);
+					}
+					if (requested_width==INT_MAX)
+					{
+						requested_width = requested_precision;
+					}
+					if (requested_precision > requested_width)
+					{
+						requested_precision = requested_width;
+					}
+					if (requested_precision > strnlen (src, requested_precision))
+					{
+						requested_precision = strnlen (src, requested_precision);
+					}
+					if ((requested_width > requested_precision) && minusflag)
+					{
+						displaychr (y, x, color, ' ', requested_width - requested_precision);
+						x += requested_width - requested_precision;
+					}
+					_displaystr (y, x, color, src, requested_precision);
+					x += requested_precision;
+					if ((requested_width > requested_precision) && !minusflag)
+					{
+						displaychr (y, x, color, ' ', requested_width - requested_precision);
+						x += requested_width - requested_precision;
+					}
+					width -= requested_width;
+					fmt++;
+					break;
+				}
+			case 'S':
+				{
+					const char *src = va_arg (ap, const char *);
+					int src_width = _measurestr_utf8 (src, strlen (src));
+					if (requested_precision==INT_MAX)
+					{
+						requested_precision = src_width;
+					}
+					if (requested_width==INT_MAX)
+					{
+						requested_width = requested_precision;
+					}
+					if (requested_precision > requested_width)
+					{
+						requested_precision = requested_width;
+					}
+					if (requested_precision > src_width)
+					{
+						requested_precision = src_width;
+					}
+					if ((requested_width > requested_precision) && minusflag)
+					{
+						displaychr (y, x, color, ' ', requested_width - requested_precision);
+						x += requested_width - requested_precision;
+					}
+					_displaystr_utf8 (y, x, color, src, requested_precision);
+					x += requested_precision;
+					if ((requested_width > requested_precision) && !minusflag)
+					{
+						displaychr (y, x, color, ' ', requested_width - requested_precision);
+						x += requested_width - requested_precision;
+					}
+					width -= requested_width;
+					fmt++;
+					break;
+				}
+			case 'd':
+				lls = va_arg (ap, signed int);
+				fmt++;
+				goto got_lls;
+			case 'u':
+				llu = va_arg (ap, unsigned int);
+				fmt++;
+				goto got_llu;
+			case 'x':
+				llu = va_arg (ap, unsigned int);
+				fmt++;
+				goto got_llx;
+			case 'X':
+				llu = va_arg (ap, unsigned int);
+				fmt++;
+				goto got_llX;
+			case 'l':
+				switch (fmt[1])
+				{
+					default:
+						va_end(ap);
+						return; /* FAILURE */
+					case 'd':
+						lls = va_arg (ap, signed long int);
+						fmt+=2;
+						goto got_lls;
+					case 'u':
+						llu = va_arg (ap, unsigned long int);
+						fmt+=2;
+						goto got_llu;
+					case 'x':
+						llu = va_arg (ap, unsigned long int);
+						fmt+=2;
+						goto got_llx;
+					case 'X':
+						llu = va_arg (ap, unsigned long int);
+						fmt+=2;
+						goto got_llX;
+					case 'l':
+					{
+						int src_width;
+						char buffer[21];
+						switch (fmt[2])
+						{
+							default:
+								va_end(ap);
+								return; /* FAILURE */
+							case 'd':
+								lls = va_arg (ap, signed long long int);
+								fmt+=3;
+						got_lls:
+								if (spaceflag)
+								{
+									snprintf (buffer, sizeof (buffer), "% lld", lls);
+								} else if (plusflag)
+								{
+									snprintf (buffer, sizeof (buffer), "%+lld", lls);
+								} else {
+									snprintf (buffer, sizeof (buffer), "%lld", lls);
+								}
+						ready_lls:
+								src_width = strlen(buffer);
+
+								if (requested_precision==INT_MAX)
+								{
+									requested_precision = src_width;
+								}
+								if (requested_width==INT_MAX)
+								{
+									requested_width = requested_precision;
+								}
+								if (requested_precision > requested_width)
+								{
+									requested_precision = requested_width;
+								}
+								if (requested_precision > src_width)
+								{
+									requested_precision = src_width;
+								}
+								if (src_width > requested_precision)
+								{
+									int i;
+									for (i=0; buffer[i]; i++)
+									{
+										if ((buffer[i] != '-') && (buffer[i] != '+') && (buffer[i] != ' '))
+										{
+											buffer[i] = '9';
+										}
+									}
+									buffer[requested_width]=0;
+									requested_precision = requested_width;
+									src_width = requested_width;
+								}
+								while ((requested_width > requested_precision) && (requested_width <= 20) && zeroflag)
+								{
+									if ((buffer[0] == '-') || (buffer[0] == '+'))
+									{
+										memmove (buffer + 2, buffer + 1, strlen (buffer + 1) + 1);
+										buffer[1] = '0';
+									} else {
+										memmove (buffer + 1, buffer, strlen (buffer) + 1);
+										buffer[0] = '0';
+									}
+								}
+								if ((requested_width > requested_precision) && minusflag)
+								{
+									displaychr (y, x, color, ' ', requested_width - requested_precision);
+									x += requested_width - requested_precision;
+								}
+								_displaystr (y, x, color, buffer, requested_precision);
+								x += requested_precision;
+								if ((requested_width > requested_precision) && !minusflag)
+								{
+									displaychr (y, x, color, ' ', requested_width - requested_precision);
+									x += requested_width - requested_precision;
+								}
+								width -= requested_width;
+								break;
+							case 'u':
+								llu = va_arg (ap, unsigned long long int);
+								fmt+=3;
+						got_llu:
+								snprintf (buffer, sizeof (buffer), "%llu", llu);
+								goto ready_lls;
+							case 'x':
+								llu = va_arg (ap, unsigned long long int);
+								fmt+=3;
+						got_llx:
+								if (hashflag)
+								{
+									snprintf (buffer, sizeof (buffer), "%#llx", llu);
+								} else {
+									snprintf (buffer, sizeof (buffer), "%llx", llu);
+								}
+						ready_llx:
+								src_width = strlen(buffer);
+								if (requested_precision==INT_MAX)
+								{
+									requested_precision = src_width;
+								}
+								if (requested_width==INT_MAX)
+								{
+									requested_width = requested_precision;
+								}
+								if (requested_precision > requested_width)
+								{
+									requested_precision = requested_width;
+								}
+								if (requested_precision > src_width)
+								{
+									requested_precision = src_width;
+								}
+								if (src_width > requested_precision)
+								{
+									int i;
+									for (i=0; buffer[i]; i++)
+									{
+										if ((buffer[i] != '-') && (buffer[i] != '+') && (buffer[i] != ' '))
+										{
+											buffer[i] = '9';
+										}
+									}
+									buffer[requested_width]=0;
+									requested_precision = requested_width;
+									src_width = requested_width;
+								}
+								while ((requested_width > requested_precision) && (requested_width <= 20) && zeroflag)
+								{
+									if ((buffer[0] != '0') && ((buffer[1] != 'x') || (buffer[1] != 'X')))
+									{
+										memmove (buffer + 3, buffer + 2, strlen (buffer + 2) + 1);
+										buffer[2] = '0';
+									} else {
+										memmove (buffer + 1, buffer, strlen (buffer) + 1);
+										buffer[0] = '0';
+									}
+								}
+								if ((requested_width > requested_precision) && minusflag)
+								{
+									displaychr (y, x, color, ' ', requested_width - requested_precision);
+									x += requested_width - requested_precision;
+								}
+								_displaystr (y, x, color, buffer, requested_precision);
+								x += requested_precision;
+								if ((requested_width > requested_precision) && !minusflag)
+								{
+									displaychr (y, x, color, ' ', requested_width - requested_precision);
+									x += requested_width - requested_precision;
+								}
+								width -= requested_width;
+								break;
+
+							case 'X':
+								llu = va_arg (ap, unsigned long long int);
+								fmt+=3;
+							got_llX:
+								if (hashflag)
+								{
+									snprintf (buffer, sizeof (buffer), "%#llX", llu);
+								} else {
+									snprintf (buffer, sizeof (buffer), "%llX", llu);
+								}
+								goto ready_llx;
+						} // switch (fmt[2])
+					} // case 'l':
+					break;
+				} // switch (fmt[1])
+			// case 'l':
+			break;
+		} // switch (fmt[0])
+	} // while
+	if (width)
+	{
+		displaychr (y, x, color, ' ', width);
+	}
+	va_end(ap);
+}
--- playtimidity/cpitimiditysetup.c
+++ playtimidity/cpitimiditysetup.c
@@ -78,16 +78,14 @@ static void TimiditySetupDrawItems (const int focus, const int lineno, const int
 	int i;
 	for (i=0; i < listlength; i++)
 	{
+		int l = strlen (list[i]);
 		if (selected == i)
 		{
-			displaystr (TimiditySetupFirstLine + lineno, xpos, (focus&&active)?0x09:0x01, "[",     1);                xpos += 1;
-			displaystr (TimiditySetupFirstLine + lineno, xpos, (focus&&active)?0x0f:0x07, list[i], strlen (list[i])); xpos += strlen (list[i]);
-			displaystr (TimiditySetupFirstLine + lineno, xpos, (focus&&active)?0x09:0x01, "]",     1);                xpos += 1;
+			display_nprintf (TimiditySetupFirstLine + lineno, xpos, (focus&&active)?0x09:0x01, l + 2, "[%.*o%s%.*o]", (focus&&active)?0x0f:0x07, list[i], (focus&&active)?0x09:0x01);
 		} else {
-			displaystr (TimiditySetupFirstLine + lineno, xpos, 0x00,                      " ",     1);                xpos += 1;
-			displaystr (TimiditySetupFirstLine + lineno, xpos, (focus&&active)?0x07:0x08, list[i], strlen (list[i])); xpos += strlen (list[i]);
-			displaystr (TimiditySetupFirstLine + lineno, xpos, 0x00,                      " ",     1);                xpos += 1;
+			display_nprintf (TimiditySetupFirstLine + lineno, xpos, 0x00, l + 2, " %.*o%s%.0o ", (focus&&active)?0x07:0x08, list[i]);
 		}
+		xpos += l + 2;
 	}
 	displaystr (TimiditySetupFirstLine + lineno, xpos, 0x00, " ", TimiditySetupWidth - xpos);
 }
@@ -100,7 +98,7 @@ static void TimiditySetupDrawBar (const int focus, const int lineno, const int s
 		int tw = TimiditySetupWidth - 5 - 16 - skip - skip - 2;
 		int pw = tw * level / maxlevel;
 		int p1, p2, p3, p4;
-	
+
 		p1 = tw * 1 / 4;
 		p2 = tw * 2 / 4;
 		p3 = tw * 3 / 4;
@@ -165,103 +163,21 @@ static void TimiditySetupDraw(int focus)
 
 	displaystr (TimiditySetupFirstLine + 2, 0, (focus&&(TimiditySetupEditPos==1))?0x07:0x08, "  Level:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawBar (focus, 2, skip, (TimiditySetupSelected != 0) ? TimiditySetupLevel : -1, 127, TimiditySetupEditPos == 1);
-#if 0
-	if (TimiditySetupSelected != 0)
-	{
-		int tw = TimiditySetupWidth - 5 - 16 - skip - skip - 2;
-		int pw = tw * TimiditySetupLevel / 127;
-		int p1, p2, p3, p4;
-
-		displaystr (TimiditySetupFirstLine + 2, 16 + skip, (focus&&(TimiditySetupEditPos==1))?0x07:0x08, "[", 1);
-	
-		p1 = tw * 1 / 4;
-		p2 = tw * 2 / 4;
-		p3 = tw * 3 / 4;
-		p4 = tw;
-		if (p1 > pw)
-		{
-			p1 = pw;
-			p2 = 0;
-			p3 = 0;
-			p4 = 0;
-		} else if (p2 > pw)
-		{
-			p2 = pw - p1;
-			p3 = 0;
-			p4 = 0;
-		} else if (p3 > pw)
-		{
-			p3 = pw - p2;
-			p2 -= p1;
-			p4 = 0;
-		} else {
-			p4 = pw - p3;
-			p3 -= p2;
-			p2 -= p1;
-		}
-		displaychr (TimiditySetupFirstLine + 2, 16 + 1 + skip,                     (focus && (TimiditySetupEditPos==1)) ?0x01:0x08, '\xfe', p1);
-		displaychr (TimiditySetupFirstLine + 2, 16 + 1 + skip + p1,                (focus && (TimiditySetupEditPos==1)) ?0x09:0x08, '\xfe', p2);
-		displaychr (TimiditySetupFirstLine + 2, 16 + 1 + skip + p1 + p2,           (focus && (TimiditySetupEditPos==1)) ?0x0b:0x08, '\xfe', p3);
-		displaychr (TimiditySetupFirstLine + 2, 16 + 1 + skip + p1 + p2 + p3,      (focus && (TimiditySetupEditPos==1)) ?0x0f:0x08, '\xfe', p4);
-		displaychr (TimiditySetupFirstLine + 2, 16 + 1 + skip + p1 + p2 + p3 + p4, (focus && (TimiditySetupEditPos==1)) ?0x07:0x08, '\xfa', tw - p1 - p2 - p3 - p4);
-
-		snprintf (temp, sizeof (temp), "]%5d", TimiditySetupLevel);
-		displaystr (TimiditySetupFirstLine + 2, TimiditySetupWidth - 6 - skip, (focus&&(TimiditySetupEditPos==1))?0x07:0x08, temp, 6 + skip);
-	} else {
-		displaystr (TimiditySetupFirstLine + 2, 16 + skip, 0x08, "----", TimiditySetupWidth - 16 - skip);
-	}
-#endif
 
 	displaystr (TimiditySetupFirstLine + 3, 0, (focus&&(TimiditySetupEditPos==2))?0x07:0x08, "  ScaleRoom:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawBar (focus, 3, skip, (TimiditySetupSelected >= 3) ? TimiditySetupScaleRoom : -1, 1000, TimiditySetupEditPos == 2);
-#if 0
-	if (TimiditySetupSelected >= 3)
-	{
-		snprintf (temp, sizeof (temp), "%4d", TimiditySetupScaleRoom);
-		displaystr (TimiditySetupFirstLine + 3, 16 + skip, (focus&&(TimiditySetupEditPos==2))?0x07:0x08, temp, TimiditySetupWidth - 16 - skip);
-	} else {
-		displaystr (TimiditySetupFirstLine + 3, 16 + skip, 0x08, "----", TimiditySetupWidth - 16 - skip);
-	}
-#endif
 
 	displaystr (TimiditySetupFirstLine + 4, 0, (focus&&(TimiditySetupEditPos==3))?0x07:0x08, "  OffsetRoom:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawBar (focus, 4, skip, (TimiditySetupSelected >= 3) ? TimiditySetupOffsetRoom : -1, 1000, TimiditySetupEditPos == 3);
-#if 0
-	if (TimiditySetupSelected >= 3)
-	{
-		snprintf (temp, sizeof (temp), "%4d", TimiditySetupOffsetRoom);
-		displaystr (TimiditySetupFirstLine + 4, 16 + skip, (focus&&(TimiditySetupEditPos==3))?0x07:0x08, temp, TimiditySetupWidth - 16 - skip);
-	} else {
-		displaystr (TimiditySetupFirstLine + 4, 16 + skip, 0x08, "----", TimiditySetupWidth - 16 - skip);
-	}
-#endif
 
 	displaystr (TimiditySetupFirstLine + 5, 0, (focus&&(TimiditySetupEditPos==4))?0x07:0x08, "  PreDelayFactor:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawBar (focus, 5, skip, (TimiditySetupSelected >= 3) ? TimiditySetupPreDelayFactor : -1, 1000, TimiditySetupEditPos == 4);
-#if 0
-	if (TimiditySetupSelected >= 3)
-	{
-		snprintf (temp, sizeof (temp), "%4d", TimiditySetupPreDelayFactor);
-		displaystr (TimiditySetupFirstLine + 5, 16 + skip, (focus&&(TimiditySetupEditPos==4))?0x07:0x08, temp, TimiditySetupWidth - 16 - skip);
-	} else {
-		displaystr (TimiditySetupFirstLine + 5, 16 + skip, 0x08, "----", TimiditySetupWidth - 16 - skip);
-	}
-#endif
 
 	displaystr (TimiditySetupFirstLine + 6, 0, (focus&&(TimiditySetupEditPos==5))?0x07:0x08, "  Delay:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawItems (focus, 6, skip, effect_lr_modes, 4, effect_lr_mode + 1, TimiditySetupEditPos==5);
 
 	displaystr (TimiditySetupFirstLine + 7, 0, (focus&&(TimiditySetupEditPos==6))?0x07:0x08, "  Delay ms:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawBar (focus, 7, skip, (effect_lr_mode >= 0) ? effect_lr_delay_msec : -1, 1000, TimiditySetupEditPos == 6);
-#if 0
-	if (effect_lr_mode >= 0)
-	{
-		snprintf (temp, sizeof (temp), "%4d", effect_lr_delay_msec);
-		displaystr (TimiditySetupFirstLine + 7, 16 + skip, (focus&&(TimiditySetupEditPos==6))?0x07:0x08, temp, TimiditySetupWidth - 16 - skip);
-	} else {
-		displaystr (TimiditySetupFirstLine + 7, 16 + skip, 0x08, "----", TimiditySetupWidth - 16 - skip);
-	}
-#endif
 
 	displaystr (TimiditySetupFirstLine + 8, 0, (focus&&(TimiditySetupEditPos==7))?0x07:0x08, "  Chorus:" + 2 - skip, 16 + skip);
 	TimiditySetupDrawItems (focus, 8, skip, disable_enable, 2, opt_chorus_control, TimiditySetupEditPos==7);
