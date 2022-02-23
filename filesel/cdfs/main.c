ccf2852cf4ccdae7c5e7899a6601051e621487cd
Stian Skjelstad <stian.skjelstad@gmail.com>
2022-02-23 07:47:15 +0100

    Add GPL headers to the new CDFS files

--- filesel/cdfs/main.c
+++ filesel/cdfs/main.c
@@ -1,3 +1,23 @@
+/* OpenCP Module Player
+ * copyright (c) 2022 Stian Skjelstad <stian.skjelstad@gmail.com>
+ *
+ * The most toplevel part of CDFS
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include "config.h"
 #include <iconv.h>
 #include <stdio.h>
@@ -424,4 +444,4 @@ static void cdfsclose(void)
 char *dllinfo = "";
 #endif
 
-DLLEXTINFO_PREFIX struct linkinfostruct dllextinfo = {.name = "cdfs", .desc = "OpenCP UNIX CDFS filebrowser (c) 2022 Stian Skjelstad", .ver = DLLVERSION, .size = 0, .Init = cdfsint, .Close = cdfsclose};
+DLLEXTINFO_PREFIX struct linkinfostruct dllextinfo = {.name = "cdfs", .desc = "OpenCP virtual CDFS filebrowser (c) 2022 Stian Skjelstad", .ver = DLLVERSION, .size = 0, .Init = cdfsint, .Close = cdfsclose};
