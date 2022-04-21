e41df808d320ed8850faa0afc9f14f849dcce8db
Stian Skjelstad <stian.skjelstad@gmail.com>
2022-04-21 23:22:45 +0200

    Update DiskWriter audio driver to the new devp API.

--- devp/devpdisk.c
+++ devp/devpdisk.c
@@ -31,6 +31,7 @@
  */
 
 #include "config.h"
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -45,50 +46,106 @@
 #include "dev/imsdev.h"
 #include "dev/player.h"
 #include "dev/plrasm.h"
+#include "dev/ringbuffer.h"
 #include "filesel/dirdb.h"
 #include "filesel/filesystem.h"
 #include "stuff/imsrtns.h"
 
 extern struct sounddevice plrDiskWriter;
 
-static uint32_t filepos;
-static unsigned long buflen;
-static char *playbuf;
-static char *shadowbuf;
-static unsigned long bufpos;
-static int file;
-static unsigned char *diskcache;
-static unsigned long cachelen;
-static unsigned long cachepos;
+static void *devpDiskBuffer;
+static struct ringbuffer_t *devpDiskRingBuffer;
+static char *devpDiskShadowBuffer;
+static int devpDiskFileHandle = -1;
+static unsigned char *devpDiskCache;
+static unsigned long devpDiskCachelen;
+static unsigned long devpDiskCachePos;
 static volatile char busy;
-static unsigned short playrate;
+static uint32_t devpDiskRate;
 static unsigned char stereo;
 static unsigned char bit16;
 static unsigned char writeerr;
 
-static void Flush(void)
+static void devpDiskConsume(int flush)
 {
+	int pos1, length1, pos2, length2;
+
+	ringbuffer_get_tail_samples (devpDiskRingBuffer, &pos1, &length1, &pos2, &length2);
+
+#define BUFFER_TO_KEEP 2048  // for visuals
+
+	if (!flush)
+	{
+		if ((length1 + length2) <= BUFFER_TO_KEEP)
+		{
+			return;
+		}
+		if (length2)
+		{
+			if (length2 < BUFFER_TO_KEEP)
+			{
+				length1 = (length2 + length1) - BUFFER_TO_KEEP;
+				length2 = 0;
+			} else {
+				length2 = length2 - BUFFER_TO_KEEP;
+			}
+		} else {
+			length1 = length1 - BUFFER_TO_KEEP;
+		}
+	}
+
+	if (devpDiskShadowBuffer)
+	{
+		plrConvertBuffer (devpDiskCache + devpDiskCachePos, (int16_t *)devpDiskBuffer + (pos1 << 1), length1, bit16 /* 16bit */, bit16 /* signed follows 16bit */, stereo, 0 /* revstereo */);
+		devpDiskCachePos += length1 << ((!!bit16) + (!!stereo));
+		if (length2)
+		{
+			plrConvertBuffer (devpDiskCache + devpDiskCachePos, (int16_t *)devpDiskBuffer + (pos2 << 1), length2, bit16 /* 16bit */, bit16 /* signed follows 16bit */, stereo, 0 /* revstereo */);
+			devpDiskCachePos += length2 << ((!!bit16) + (!!stereo));
+		}
+	} else {
+		memcpy(devpDiskCache + devpDiskCachePos, (uint8_t *)devpDiskBuffer + (pos1 << 2), length1 << 2);
+		devpDiskCachePos += (length1 << 2); /* stereo + bit16 */
+		if (length2)
+		{
+			memcpy(devpDiskCache + devpDiskCachePos, (uint8_t *)devpDiskBuffer + (pos2 << 2), length2 << 2);
+			devpDiskCachePos += (length2 << 2); /* stereo + bit16 */
+		}
+	}
+
+	ringbuffer_tail_consume_samples (devpDiskRingBuffer, length1 + length2);
+
+	assert (devpDiskCachePos <= devpDiskCachelen);
+}
+
+static unsigned int devpDiskIdle(void)
+{
+	unsigned int retval;
+
 	busy++;
 	if (busy != 1)
 	{
 		busy--;
-		return;
+		return 0;
 	}
-	if (cachepos > (cachelen/2))
+
+	devpDiskConsume (0);
+
+	if (devpDiskCachePos > (devpDiskCachelen/2))
 	{
 		if (!writeerr)
 		{
 			if (bit16)
 			{
-				int i, j = cachepos/2;
-				uint16_t *d = (uint16_t *)diskcache;
+				int i, j = devpDiskCachePos/2;
+				uint16_t *d = (uint16_t *)devpDiskCache;
 				for (i=0;i<j;i++)
 				{
 					d[i]=uint16_little(d[i]);
 				}
 			}
 rewrite:
-			if ((unsigned)write(file, diskcache, cachepos)!=cachepos)
+			if ((unsigned)write(devpDiskFileHandle, devpDiskCache, devpDiskCachePos) != devpDiskCachePos)
 			{
 				if (errno==EAGAIN)
 					goto rewrite;
@@ -97,153 +154,118 @@ rewrite:
 				writeerr=1;
 			}
 		}
-		filepos += cachepos;
-		cachepos = 0;
+		devpDiskCachePos = 0;
 	}
-	busy=0;
+
+	retval = ringbuffer_get_tail_available_samples (devpDiskRingBuffer);
+
+	busy--;
+
+	return retval;
 }
 
-static int getbufpos(void)
-{
-	if (cachepos > (cachelen/2)) return bufpos;
-	return (buflen + bufpos - (1<<(stereo+bit16))) % buflen;
-}
-
-static int getplaypos(void)
-{
-	return bufpos;
-}
-
-static void advance(unsigned int pos)
+static void devpDiskCommitBuffer (unsigned int samples)
 {
 	busy++;
 
-	if (pos == bufpos)
+	if (!samples)
 	{
 		return;
 	}
 
-	if (shadowbuf)
-	{
-		plrConvertBuffer ((int16_t *)playbuf, shadowbuf, buflen, bufpos, pos, bit16 /* 16bit */, bit16 /* signed follows 16bit */, stereo, 0 /* revstereo */);
-	}
-
-	if (shadowbuf)
-	{
-		unsigned int _buflen = buflen >> ((!bit16)+(!stereo));
-		unsigned int _bufpos = bufpos >> ((!bit16)+(!stereo));
-		unsigned int _pos = pos >> ((!bit16)+(!stereo));
-
-		if (_pos < _bufpos)
-		{
-			memcpy(diskcache + cachepos                   , shadowbuf + _bufpos, _buflen - _bufpos);
-			memcpy(diskcache + cachepos + _buflen -_bufpos, shadowbuf          ,  _pos);
-			cachepos += _buflen - _bufpos + _pos;
-#if 0
-		} else if (_pos == _bufpos) /* is this legal? */
-		{
-			fprintf (stderr, "advance _pos %d == _bufpos %d   ", _pos, _bufpos);
-			memcpy(diskcache + cachepos                    , shadowbuf + _bufpos, _buflen - _bufpos);
-			memcpy(diskcache + cachepos + _buflen - _bufpos, shadowbuf          , _pos);
-			cachepos += _buflen;
-#endif
-		} else {
-			memcpy(diskcache + cachepos, shadowbuf + _bufpos, _pos - _bufpos);
-			cachepos += _pos - _bufpos;
-		}
-	} else {
-		if (pos<bufpos)
-		{
-			memcpy(diskcache + cachepos                  , playbuf + bufpos, buflen - bufpos);
-			memcpy(diskcache + cachepos + buflen - bufpos, playbuf         , pos);
-			cachepos += buflen-bufpos +pos;
-#if 0
-		} else if (pos == bufpos) /* is this legal? */
-		{
-			memcpy(diskcache + cachepos                  , playbuf + bufpos, buflen - bufpos);
-			memcpy(diskcache + cachepos + buflen - bufpos, playbuf         , pos);
-			cachepos += buflen;
-#endif
-		} else {
-			memcpy(diskcache + cachepos, playbuf + bufpos, pos - bufpos);
-			cachepos += pos - bufpos;
-		}
-	}
-
-	if (cachepos > cachelen)
-	{
-		fprintf(stderr, "devpdisk: cachepos>cachelen\n");
-		exit(0);
-	}
-
-	bufpos=pos;
+	ringbuffer_head_add_samples (devpDiskRingBuffer, samples);
 
 	busy--;
 }
 
-static uint32_t gettimer(void)
+static void devpDiskGetBuffer (void **buf, unsigned int *samples)
 {
-	return imuldiv((filepos+cachepos), (65536>>(stereo+bit16)), playrate);
+	int pos1, length1;
+	assert (devpDiskRingBuffer);
+
+	ringbuffer_get_head_samples (devpDiskRingBuffer, &pos1, &length1, 0, 0);
+
+	*samples = length1;
+	*buf = (uint8_t *)devpDiskBuffer + (pos1<<2); /* stereo + bit16 */
 }
 
-static void dwSetOptions(uint32_t rate, int opt)
+static uint32_t devpDiskGetRate (void)
 {
+	return devpDiskRate;
+}
+
+static void devpDiskOnBufferCallback (int samplesuntil, void (*callback)(void *arg, int samples_ago), void *arg)
+{
+	assert (devpDiskRingBuffer);
+	ringbuffer_add_tail_callback_samples (devpDiskRingBuffer, samplesuntil, callback, arg);
+}
+
+static int devpDiskPlay (uint32_t *rate, enum plrRequestFormat *format, struct ocpfilehandle_t *source_file)
+{
+	int plrbufsize; /* given in ms */
+	int buflength;
+
 	stereo = !cfGetProfileBool("commandline_s", "m", !cfGetProfileBool("devpDisk", "stereo", 1, 1), 1);
 	bit16 =  !cfGetProfileBool("commandline_s", "8", !cfGetProfileBool("devpDisk", "16bit", 1, 1), 1);
 
-	if (rate<5000)
-		rate=5000;
-
-	if (rate>96000)
-		rate=96000;
-
-	playrate=rate;
-	plrRate=rate;
-	plrOpt=PLR_STEREO_16BIT_SIGNED;
-}
-
-static int dwPlay(void **buf, unsigned int *len, struct ocpfilehandle_t *source_file)
-{
-	unsigned char hdr[0x2C];
-
-	memset(&hdr, 0, sizeof(hdr));
-
-	if (*len>32704)
-		*len=32704;
-	buflen=*len;
-	*buf=playbuf=malloc(*len);
-	if (!*buf)
+	if (*rate == 0)
 	{
-		fprintf (stderr, "dwPlay(): malloc() failed #1\n");
-		return 0;
+		*rate = 44100;
+	}
+	if (*rate<5000)
+	{
+		*rate=5000;
+	}
+	if (*rate>96000)
+	{
+		*rate=96000;
+	}
+	devpDiskRate = *rate;
+	*format=PLR_STEREO_16BIT_SIGNED; // Fixed
+
+	plrbufsize = cfGetProfileInt2(cfSoundSec, "sound", "plrbufsize", 1000, 10);
+	/* clamp the plrbufsize to be atleast 1000ms and below 2000 ms */
+	if (plrbufsize < 1000)
+	{
+		plrbufsize = 1000;
+	}
+	if (plrbufsize > 2000)
+	{
+		plrbufsize = 2000;
+	}
+	buflength = devpDiskRate * plrbufsize / 1000;
+
+	devpDiskBuffer=calloc(buflength, 4);
+	if (!devpDiskBuffer)
+	{
+		fprintf (stderr, "[devpDisk]: malloc() failed #1\n");
+		goto error_out;
+	}
+	devpDiskRingBuffer = ringbuffer_new_samples (RINGBUFFER_FLAGS_STEREO | RINGBUFFER_FLAGS_16BIT | RINGBUFFER_FLAGS_SIGNED, buflength);
+	if (!devpDiskRingBuffer)
+	{
+		fprintf (stderr, "[devpDisk]: ringbuffer_new_samples() failed\n");
+		goto error_out;
 	}
 	if ((!bit16) || (!stereo))
 	{ /* we need to convert the signal format... */
-		shadowbuf = malloc ( buflen >> ((!bit16) + (!stereo)));
-		if (!shadowbuf)
+		devpDiskShadowBuffer = malloc ( buflength << ((bit16) + (stereo)));
+		if (!devpDiskShadowBuffer)
 		{
-			fprintf (stderr, "dwPlay(): malloc() failed #2\n");
-			free (playbuf);
-			playbuf=0; *buf=0;
-			return 0;
+			fprintf (stderr, "[devpDisk]: malloc() failed #2\n");
+			goto error_out;
 		}
 	}
 
 	writeerr=0;
 
-	cachelen = 8*playrate;
-	if (cachelen< (2 * (*len) + 1024) )
+	devpDiskCachelen = 12*devpDiskRate; /* 3 seconds */
+	devpDiskCachePos=0;
+	devpDiskCache=calloc(devpDiskCachelen, 1);
+	if (!devpDiskCache)
 	{
-		cachelen=(2 * (*len) + 1024);
-	}
-	cachepos=0;
-	diskcache=malloc(cachelen);
-	if (!diskcache)
-	{
-		free (playbuf);
-		free (shadowbuf);
-		shadowbuf = 0; playbuf=0; *buf=0;
-		return 0;
+		fprintf (stderr, "[devpDisk]: malloc() failed #3\n");
+		goto error_out;
 	}
 
 	{
@@ -267,25 +289,24 @@ static int dwPlay(void **buf, unsigned int *len, struct ocpfilehandle_t *source_
 			} else {
 				sprintf (fn, "%s.wav", orig);
 			}
-			if ((file=open(fn, O_WRONLY|O_CREAT|O_EXCL, S_IREAD|S_IWRITE))>=0)
+			if ((devpDiskFileHandle=open(fn, O_WRONLY|O_CREAT|O_EXCL, S_IREAD|S_IWRITE))>=0)
 				break;
 		}
 		free (fn);
 	}
 
-	if (file<0)
-
+	if (devpDiskFileHandle<0)
 	{
-		free (playbuf);
-		free (shadowbuf);
-		free (diskcache);
-		diskcache = 0; shadowbuf = 0; playbuf=0; *buf=0;
-		return 0;
+		fprintf (stderr, "[devpDisk]: Failed to open output file\n");
+		goto error_out;
 	}
 
 	while (1)
 	{
-		if (write(file, hdr, 0x2C)>=0)
+		unsigned char hdr[0x2C];
+		memset(&hdr, 0, sizeof(hdr));
+
+		if (write(devpDiskFileHandle, hdr, 0x2C)>=0)
 			break;
 		if (errno==EAGAIN)
 			continue;
@@ -294,20 +315,24 @@ static int dwPlay(void **buf, unsigned int *len, struct ocpfilehandle_t *source_
 		break;
 	}
 
-	bufpos=0;
 	busy=0;
-	filepos=0;
-
-	plrGetBufPos=getbufpos;
-	plrGetPlayPos=getplaypos;
-	plrAdvanceTo=advance;
-	plrIdle=Flush;
-	plrGetTimer=gettimer;
 
 	return 1;
+
+error_out:
+	free (devpDiskBuffer);       devpDiskBuffer = 0;
+	free (devpDiskShadowBuffer); devpDiskShadowBuffer = 0;
+	free (devpDiskCache);        devpDiskCache = 0;
+	if (devpDiskRingBuffer)
+	{
+		ringbuffer_free (devpDiskRingBuffer);
+		devpDiskRingBuffer = 0;
+	}
+
+	return 0;
 }
 
-static void dwStop(void)
+static void devpDiskStop(void)
 {
 	uint32_t wavlen;
 	struct __attribute__((packed))
@@ -327,21 +352,26 @@ static void dwStop(void)
 		uint32_t wavlen;
 	} wavhdr;
 
-	plrIdle=0;
+	if (devpDiskFileHandle < 0)
+	{
+		return;
+	}
+
+	devpDiskConsume (1);
 
 	if (!writeerr)
 	{
 		if (bit16)
 		{
-			int i, j = cachepos/2;
-			uint16_t *d = (uint16_t *)diskcache;
+			int i, j = devpDiskCachePos/2;
+			uint16_t *d = (uint16_t *)devpDiskCache;
 			for (i=0;i<j;i++)
 			{
 				d[i]=uint16_little(d[i]);
 			}
 		}
 rewrite:
-		if (write(file, diskcache, cachepos)<0)
+		if (write (devpDiskFileHandle, devpDiskCache, devpDiskCachePos)<0)
 		{
 			if (errno==EINTR)
 				goto rewrite;
@@ -350,9 +380,9 @@ rewrite:
 		}
 	}
 
-	wavlen=lseek(file, 0, SEEK_CUR)-0x2C;
+	wavlen = lseek (devpDiskFileHandle, 0, SEEK_CUR)-0x2C;
 
-	lseek(file, 0, SEEK_SET);
+	lseek (devpDiskFileHandle, 0, SEEK_SET);
 
 	memcpy(wavhdr.riff, "RIFF", 4);
 	memcpy(wavhdr.wave, "WAVE", 4);
@@ -361,14 +391,14 @@ rewrite:
 	wavhdr.chlen=uint32_little(0x10);
 	wavhdr.form=uint16_little(1);
 	wavhdr.chan=uint16_little(1<<stereo);
-	wavhdr.rate=uint32_little(playrate);
+	wavhdr.rate=uint32_little(devpDiskRate);
 	wavhdr.bits=uint16_little(8<<bit16);
 	wavhdr.bpsmp=uint16_little((1<<stereo)*(8<<bit16)/8);
-	wavhdr.datarate=uint32_little(((1<<stereo)*(8<<bit16)/8)*playrate);
+	wavhdr.datarate=uint32_little(((1<<stereo)*(8<<bit16)/8)*devpDiskRate);
 	wavhdr.wavlen=uint32_little(wavlen);
 	wavhdr.len24=uint32_little(wavlen+0x24);
 rewrite2:
-	if (write(file, &wavhdr, 0x2C)<0)
+	if (write (devpDiskFileHandle, &wavhdr, 0x2C)<0)
 	{
 		if (errno==EINTR)
 			goto rewrite2;
@@ -376,32 +406,83 @@ rewrite2:
 			goto rewrite2;
 	}
 
-	lseek(file, 0, SEEK_END);
+	lseek (devpDiskFileHandle, 0, SEEK_END);
 reclose:
-	if (close(file)<0)
+	if (close (devpDiskFileHandle) < 0)
 	{
 		if (errno==EINTR)
 			goto reclose;
 	}
-	free(playbuf);
-	free(shadowbuf);
-	free(diskcache);
-	playbuf=0;
-	shadowbuf=0;
-	diskcache=0;
+	free(devpDiskBuffer);
+	free(devpDiskShadowBuffer);
+	free(devpDiskCache);
+
+	if (devpDiskRingBuffer)
+	{
+		ringbuffer_reset (devpDiskRingBuffer);
+		ringbuffer_free (devpDiskRingBuffer);
+		devpDiskRingBuffer = 0;
+	}
+
+	devpDiskBuffer = 0;
+	devpDiskShadowBuffer = 0;
+	devpDiskCache = 0;
+	devpDiskFileHandle = -1;
 }
+
+static void devpDiskPeekBuffer (void **buf1, unsigned int *buf1length, void **buf2, unsigned int *buf2length)
+{
+	int pos1, length1, pos2, length2;
+
+	ringbuffer_get_tail_samples (devpDiskRingBuffer, &pos1, &length1, &pos2, &length2);
+
+	if (length1)
+	{
+		*buf1 = (uint8_t *)devpDiskBuffer + (pos1 << 2); /* stereo + 16bit */
+		*buf1length = length1;
+		if (length2)
+		{
+			*buf2 = (uint8_t *)devpDiskBuffer + (pos2 << 2); /* stereo + 16bit */
+			*buf2length = length2;
+		} else {
+			*buf2 = 0;
+			*buf2length = 0;
+		}
+	} else {
+		*buf1 = 0;
+		*buf1length = 0;
+		*buf2 = 0;
+		*buf2length = 0;
+	}
+}
+static void devpDiskPause (int pause)
+{ /* a no-op on devpDisk */
+}
+
+static const struct plrAPI_t devpDisk = {
+	devpDiskIdle,
+	devpDiskPeekBuffer,
+	devpDiskPlay,
+	devpDiskGetBuffer,
+	devpDiskGetRate,
+	devpDiskOnBufferCallback,
+	devpDiskCommitBuffer,
+	devpDiskPause,
+	devpDiskStop
+};
 
 static int dwInit(const struct deviceinfo *d)
 {
-	plrSetOptions=dwSetOptions;
-	plrPlay=dwPlay;
-	plrStop=dwStop;
+	plrAPI=&devpDisk;
 	return 1;
 }
 
 static void dwClose(void)
 {
-	plrPlay=0;
+	if (plrAPI  == &devpDisk)
+	{
+		plrAPI = 0;
+	}
 }
 
 static int dwDetect(struct deviceinfo *card)
