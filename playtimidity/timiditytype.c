2008e67c481b09161165521c4336828fef765d07
Stian Skjelstad <stian.skjelstad@gmail.com>
2022-11-15 14:26:15 +0100

    * Add support for MIDI files that ends with .KAR * Try to extract song title from Soft Karaoke MIDI files.
    Fixes #75

--- playtimidity/timiditytype.c
+++ playtimidity/timiditytype.c
@@ -37,80 +37,200 @@
 #include "stuff/err.h"
 #include "timiditytype.h"
 
-static uint32_t timidityGetModuleType (const uint8_t *buf)
-{
-	if (*(uint32_t*)buf==uint32_little(0x6468544D)) /* "MThd"  - midi without a RIFF container */
-		return MODULETYPE("MIDI");
-	if ((*(uint32_t*)buf==uint32_little(0x46464952))&&(*(uint32_t*)(buf+8)==uint32_little(0x44494D52))) /* "RIFF" "RMID" - RIFF container for MIDI */
-		return MODULETYPE("MIDI");
-
-	return 0;
-}
-
 static int timidityReadInfo(struct moduleinfostruct *m, struct ocpfilehandle_t *fp, const char *_buf, size_t flen, const struct mdbReadInfoAPI_t *API)
 {
 	const uint8_t *buf = (const uint8_t *)_buf;
-	int type;
+	uint16_t mtype;
 	unsigned int i;
-	uint32_t len;
+	uint32_t chunklen;
+	int IsSoftKaraoke = 0;
+	int TrackNo = 0;
 
 	if (flen<12)
 	{
 		return 0;
 	}
 
-	if (!(type=timidityGetModuleType(buf)))
+	/* if RIFF container is present, verify RMID and locate the data, offset "i" and limit "flen" */
+	if (!memcmp(buf, "RIFF", 4))
+	{
+		if (memcmp (buf + 8, "RMID", 4))
+		{ /* not a RMID, bail out */
+			return 0;
+		}
+		buf += 12; flen -= 12; /* skip the main RIFF header */
+
+		while (1)
+		{ /* and locate the data chunk */
+			if (flen < 8)
+			{ /* data-chunk not found */
+				return 0;
+			}
+			chunklen = (buf[4]<<24)|(buf[5]<<16)|(buf[6]<<8)|(buf[7]);
+			if (!memcmp (buf, "data", 4))
+			{
+				buf += 8; flen -= 8;
+				if (flen > chunklen)
+				{
+					flen = chunklen;
+				}
+				break;
+			}
+			buf += 8; flen -= 8;
+			if (flen < chunklen)
+			{
+				return 0;
+			}
+			buf += chunklen + (chunklen & 1); /* skip to the next chunk, and chunk datasize should be rounded up to 16bit alignment */
+			flen -= chunklen + (chunklen & 1);
+		}
+	}
+
+	/* verify MThd */
+	if ((flen < 22) || memcmp(buf, "MThd", 4))
 	{
 		return 0;
 	}
-	m->modtype.integer.i=type;
+	chunklen = (buf[4]<<24)|(buf[5]<<16)|(buf[6]<<8)|(buf[7]);
+	buf += 8; flen -= 8;
 
-	len=0;
+	if (chunklen < 6)
+	{
+		return 0;
+	}
+	mtype = (buf[0]<<8)|(buf[1]);
+	if (flen < chunklen)
+	{
+		return 0;
+	}
+	buf += chunklen; flen -= chunklen;
+
+	/* This is MIDI file */
+	m->modtype.integer.i = MODULETYPE("MIDI");
 	m->channels=16;
 
-	i=0;
-	/* if RIFF container is present, locate the MIDI data */
-	if (*(uint32_t*)buf==uint32_little(0x46464952)) /* RIFF */
+	while (1)
 	{
-		i=12;
-		while ((i+8) < flen)
+		char CurrentTrackName[256];
+		CurrentTrackName[0] = 0;
+
+		if ((TrackNo == 1) && mtype != 1) /* not multitrack format */
 		{
-			i+=8;
-			if (*(uint32_t*)(buf+i-8)==uint32_little(0x61746164)) /* data */
+			return 1;
+		}
+
+		while (1)
+		{
+			int MTrk;
+			if (flen < 8)
+			{
+				return 1;
+			}
+			chunklen = (buf[4]<<24)|(buf[5]<<16)|(buf[6]<<8)|(buf[7]);
+			MTrk = !memcmp(buf, "MTrk", 4);
+			buf += 8; flen -= 8;
+			if (chunklen > flen) chunklen = flen;
+			if (MTrk)
+			{
+				TrackNo++;
 				break;
-			i+=uint32_little(*(uint32_t*)(buf+i-4));
+			}
+			buf += chunklen; flen -= chunklen;
 		}
-	}
-	while ((i+8) < flen)
-	{
-		i+=8;
-		len=(buf[i-4]<<24)|(buf[i-3]<<16)|(buf[i-2]<<8)|(buf[i-1]);
-		if (!memcmp(buf+i-8, "MTrk", 4))
-			break;
-		i+=len;
-	}
-	len+=i;
 
-	while (((i+4)<flen) && (i+4 < len))
-	{
-		int datalen;
+		i = 0;
+		while ((i+4) < chunklen)
+		{
+			uint16_t eventType = (buf[i+1]<<8) | buf[i+0];
+			/* We only visit the initial META events */
+			if (eventType != 0xff00)
+			{
+				if (TrackNo != 1)
+				{ /* We only care about CurrentTrackName if it is track 1 or a track that is pure META events */
+					CurrentTrackName[0] = 0;
+				}
+				break;
+			}
 
-		/* Is the next even a META event, if not give up */
-		if (*(uint16_t*)(buf+i)!=uint16_little(0xFF00))
-			break;
-		/* the the META event 0x03 Sequence/Track Name, else skip it */
-		if (buf[i+2]!=0x03)
-		{
-			i+=4+buf[i+3];
-			continue;
+			switch (IsSoftKaraoke)
+			{
+				case 0: /* Find META event 0x03 Sequence/Track Name, and verify if it is "Soft Karaoke", if we are in Track 2 */
+					if (buf[i + 2] == 0x03)
+					{
+						snprintf (CurrentTrackName, sizeof (CurrentTrackName), "%.*s", buf[i + 3], (char *)buf + i + 4);
+						if ((TrackNo == 2) && !strcasecmp (CurrentTrackName, "Soft Karaoke"))
+						{
+							IsSoftKaraoke = 1;
+						}
+					}
+					break;
+
+				case 1: /* Find META event 0x01 Text, that starts with @T, if we are in Track 3 */
+					if ((buf[i + 2] == 0x01) && (TrackNo == 3))
+					{
+						if ((4 + i + buf[i + 3]) > chunklen)
+						{
+							return 1;
+						}
+						if ((buf[i + 3] >= 2) && !memcmp (buf + i + 4, "@T", 2))
+						{
+							API->cp437_f_to_utf8_z ((char *)buf + i + 4 + 2, buf[i + 3] - 2, m->title, sizeof (m->title));
+							IsSoftKaraoke = 2;
+						}
+					}
+					break;
+				case 2: /* Find META event 0x01 Text, that starts with @T, if we are in Track 3 */
+					if ((buf[i + 2] == 0x01) && (TrackNo == 3))
+					{
+						if ((4 + i + buf[i + 3]) > chunklen)
+						{
+							return 1;
+						}
+						if ((buf[i + 3] >= 2) && !memcmp (buf + i + 4, "@T", 2))
+						{
+							API->cp437_f_to_utf8_z ((char *)buf + i + 4 + 2, buf[i + 3] - 2, m->artist, sizeof (m->artist));
+							IsSoftKaraoke = 3;
+						}
+					}
+					break;
+				case 3: /* Find META event 0x01 Text, that starts with @T, if we are in Track 3 */
+					if ((buf[i + 2] == 0x01) && (TrackNo == 3))
+					{
+						if ((4 + i + buf[i + 3]) > chunklen)
+						{
+							return 1;
+						}
+						if ((buf[i + 3] >= 2) && !memcmp (buf + i + 4, "@T", 2))
+						{
+							API->cp437_f_to_utf8_z ((char *)buf + i + 4 + 2, buf[i + 3] - 2, m->comment, sizeof (m->comment));
+							return 1; /* IsSoftKaraokee = 4; */
+						}
+					}
+					break;
+			}
+			i += 4 + buf[i + 3];
 		}
-		datalen=buf[i+3];
-		if ((i + datalen + 4 <= flen) && (i + datalen + 4 < len))
+
+		if (!IsSoftKaraoke)
 		{
-			API->cp437_f_to_utf8_z ((char *)buf+i+4, datalen, m->title, sizeof (m->title));
+			if (CurrentTrackName[0])
+			{
+				API->cp437_f_to_utf8_z (CurrentTrackName, strlen (CurrentTrackName), m->title, sizeof (m->title));
+				if (TrackNo != 1)
+				{
+					return 1; /* Track 1 and later, it is a pure META, do not try fetch more */
+				}
+			}
+		} else {
+			if (TrackNo >= 3)
+			{
+				return 1; /* We only scan Track number 3 for text fields that start with @T and then bail out */
+			}
 		}
-		break;
+
+		buf += chunklen; flen -= chunklen;
 	}
+
 	return 1;
 }
 
@@ -135,6 +255,7 @@ int __attribute__ ((visibility ("internal"))) timidity_type_init (struct PluginI
 	API->fsRegisterExt ("MID");
 	API->fsRegisterExt ("MIDI");
 	API->fsRegisterExt ("RMI");
+	API->fsRegisterExt ("KAR");
 
 	mt.integer.i = MODULETYPE("MIDI");
 	API->fsTypeRegister (mt, MIDI_description, "plOpenCP", &timidityPlayer);
