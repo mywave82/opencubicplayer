0829bc8d23b3e9b105b0028eda42d5b36788bdb4
Stian Skjelstad <stian.skjelstad@gmail.com>
2023-02-24 22:00:36 +0100

    Locating the tail AVDP was off by one

--- filesel/cdfs/udf.c
+++ filesel/cdfs/udf.c
@@ -3987,11 +3987,11 @@ void __attribute__ ((visibility ("internal"))) UDF_Descriptor (struct cdfs_disc_
 		debug_printf ("SearchEnd=%" PRId32 "\n", SearchEnd);
 		if (SearchEnd > 256)
 		{
-			if (cdfs_fetch_absolute_sector_2048 (disc, SearchEnd - 1, buffer))
+			if (cdfs_fetch_absolute_sector_2048 (disc, SearchEnd, buffer))
 			{
 				debug_printf ("Failed to fetch sector N");
 			} else {
-				invalid_N = AnchorVolumeDescriptorPointer (n, buffer, SearchEnd - 1, &MainVolumeDescriptorSequenceExtent_N, &ReserveVolumeDescriptorSequenceExtent_N);
+				invalid_N = AnchorVolumeDescriptorPointer (n, buffer, SearchEnd, &MainVolumeDescriptorSequenceExtent_N, &ReserveVolumeDescriptorSequenceExtent_N);
 			}
 
 			if (cdfs_fetch_absolute_sector_2048 (disc, SearchEnd - 256, buffer))
