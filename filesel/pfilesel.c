e59773cc6c84e079563edceb934e04fc9be40440
Stian Skjelstad <stian.skjelstad@gmail.com>
2024-04-15 13:13:41 +0200

    If a file was unable to be accessed, pressing ENTER on it would cause a NULL-pointer dereference (Problem introduced in v0.2.102, adding support for libancient)

--- filesel/pfilesel.c
+++ filesel/pfilesel.c
@@ -815,12 +815,15 @@ int fsGetPrevFile (struct moduleinfostruct *info, struct ocpfilehandle_t **fileh
 	{
 		if (m->file)
 		{
-			struct ocpfilehandle_t *ancient;
 			*filehandle = m->file->open (m->file);
-			if ((ancient = ancient_filehandle (0, 0, *filehandle)))
+			if (*filehandle)
 			{
-				(*filehandle)->unref (*filehandle);
-				*filehandle = ancient;
+				struct ocpfilehandle_t *ancient;
+				if ((ancient = ancient_filehandle (0, 0, *filehandle)))
+				{
+					(*filehandle)->unref (*filehandle);
+					*filehandle = ancient;
+				}
 			}
 		}
 
@@ -893,12 +896,15 @@ int fsGetNextFile (struct moduleinfostruct *info, struct ocpfilehandle_t **fileh
 
 	if (m->file)
 	{
-		struct ocpfilehandle_t *ancient;
 		*filehandle = m->file->open (m->file);
-		if ((ancient = ancient_filehandle (0, 0, *filehandle)))
+		if (*filehandle)
 		{
-			(*filehandle)->unref (*filehandle);
-			*filehandle = ancient;
+			struct ocpfilehandle_t *ancient;
+			if ((ancient = ancient_filehandle (0, 0, *filehandle)))
+			{
+				(*filehandle)->unref (*filehandle);
+				*filehandle = ancient;
+			}
 		}
 	}
 
